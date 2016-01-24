9b38c44e077807f2d7a457782fd171873832fe70
Adri <adrigarry@gmail.com>
2016-01-25 00:03:13 +0100

    retablissement jukebox

--- pgm/lib/jukebox.js
+++ pgm/lib/jukebox.js
@@ -11,7 +11,6 @@ var self = this;
 self.loop = function(message){
 	utils.mute();
 	console.log('Let\'s play the Jukebox in loop mode !');
-	utils.sleepNode(0.5,0);
 	var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/jukebox.sh']);
 };
 
