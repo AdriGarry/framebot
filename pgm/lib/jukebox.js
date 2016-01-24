1077ea26afbdafaa23ffe04ca9f520d43aa54897
Adri <adrigarry@gmail.com>
2016-01-24 20:57:27 +0100

    jukebox jump

--- pgm/lib/jukebox.js
+++ pgm/lib/jukebox.js
@@ -4,10 +4,13 @@
 var jukebox = function(){
 
 var spawn = require('child_process').spawn;
+var _utils = require('./utils.js');
+var utils = new _utils();
 
 var self = this;
 
 self.loop = function(message){
+	utils.mute();
 	console.log('Let\'s play the Jukebox in loop mode !');
 	var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/jukebox.sh']);
 };
