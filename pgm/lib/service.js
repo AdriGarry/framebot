ad8274753338fcc0de4d03ab7da9bdf246c9b9c4
Adri <adrigarry@gmail.com>
2016-01-30 21:07:23 +0100

    service

--- pgm/lib/service.js
+++ pgm/lib/service.js
@@ -15,31 +15,31 @@ var weather = function(){
 		headers: {'Content-Type': 'text/plain'}
 	},
 	function (error, response, body){
-	if(error){
-		console.error('Error Exporting Log  /!\\');	
-	}else if(!error && response.statusCode == 200){
-		xmlreader.read(response, function (err, res){
-			if(err) return console.log(err);
-			// use .text() to get the content of a node: 
-			console.log(res.response.text());
-			// use .attributes() to get the attributes of a node: 
-			console.log(res.response.attributes().shop);
-		 
-			// using the .count() and the .at() function, you can loop through nodes with the same name: 
-			for(var i = 0; i < res.response.who.count(); i++){
-				console.log(res.response.who.at(i).text());
-			}
-		 
-			// you can also use .each() to loop through the nodes of the same name: 
-			res.response.who.each(function (i, who){
-				console.log(who.text());
-			});
-			console.log(res.response.who.at(1).text());
+		if(error){
+			console.error('Error Exporting Log  /!\\');	
+		}else if(!error && response.statusCode == 200){
+			xmlreader.read(response, function (err, res){
+				if(err) return console.log(err);
+				// use .text() to get the content of a node: 
+				console.log(res.response.text());
+				// use .attributes() to get the attributes of a node: 
+				console.log(res.response.attributes().shop);
+			 
+				// using the .count() and the .at() function, you can loop through nodes with the same name: 
+				for(var i = 0; i < res.response.who.count(); i++){
+					console.log(res.response.who.at(i).text());
+				}
+			 
+				// you can also use .each() to loop through the nodes of the same name: 
+				res.response.who.each(function (i, who){
+					console.log(who.text());
+				});
+				console.log(res.response.who.at(1).text());
 
-			// you can also get the parent of a node using .parent(): 
-			console.log(res.response.who.at(1).parent().attributes().id);
-		});
+				// you can also get the parent of a node using .parent(): 
+				console.log(res.response.who.at(1).parent().attributes().id);
+			});
+		}
 	}
-	
 }
 exports.weather = weather;
