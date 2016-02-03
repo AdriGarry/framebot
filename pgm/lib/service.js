09b017e4383d131d790d481aeed6c7531448abf4
Adri <adrigarry@gmail.com>
2016-02-04 00:51:28 +0100

    degres

--- pgm/lib/service.js
+++ pgm/lib/service.js
@@ -89,7 +89,7 @@ exports.info = info;
 var cpuTemp = function(){
 	var temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
 	temperature = ((temperature/1000).toPrecision(2));
-	console.log('Service CPU Temperature...  ' + temperature + ' degrés');
-	tts.speak('fr', 'Mon processeur est a ' + temperature + ' degrés')
+	console.log('Service CPU Temperature...  ' + temperature + ' degres');
+	tts.speak('fr', 'Mon processeur est a ' + temperature + ' degres')
 };
 exports.cpuTemp = cpuTemp;
