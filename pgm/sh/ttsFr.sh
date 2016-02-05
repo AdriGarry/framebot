2a7271206414a8db9fb607ab7c05a10c7d56723c
Adri <adrigarry@gmail.com>
2016-02-06 00:40:11 +0100

    22

--- pgm/sh/ttsFr.sh
+++ pgm/sh/ttsFr.sh
@@ -8,4 +8,4 @@ url="http://translate.google.com/translate_tts?tl=fr&client=tw-ob&q=$*"
 
 sudo amixer cset numid=3 1
 #sudo mplayer -volume 400 -really-quiet -noconsolecontrols "$url"
-sudo mplayer -softvol -softvol-max 300 -really-quiet -noconsolecontrols "$url"
+sudo mplayer -softvol -softvol-max 300 -noconsolecontrols "$url"
