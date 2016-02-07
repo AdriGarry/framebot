b0a3df9b74c62bef6546b3bb83edff4fd67bd6c5
Adri <adrigarry@gmail.com>
2016-02-07 02:04:56 +0100

    2.8.6

--- pgm/lib/tts.js
+++ pgm/lib/tts.js
@@ -11,37 +11,43 @@ var content;
 var messages = '/home/pi/odi/pgm/data/ttsMessages.properties';
 
 var speak = function(lg, txt){
-	if(txt == '' || txt === 'undefined'){		
-		content = fs.readFileSync(messages, 'UTF-8').toString().split('\n'); // \r\n
-		var rdmMax = content.length;
-		var rdmNb = ((Math.floor(Math.random()*rdmMax)));
-		txt = content[rdmNb];
-		console.log('Random speech : ' + rdmNb + '/' + rdmMax);
-		txt = txt.split(';');
-		lg = txt[0];
-		txt = txt[1];
+utils.testConnexion(function(connexion){
+	if(connexion == true){
+		if(txt == '' || txt === 'undefined'){
+			content = fs.readFileSync(messages, 'UTF-8').toString().split('\n'); // \r\n
+			var rdmMax = content.length;
+			var rdmNb = ((Math.floor(Math.random()*rdmMax)));
+			txt = content[rdmNb];
+			console.log('Random speech : ' + rdmNb + '/' + rdmMax);
+			txt = txt.split(';');
+			lg = txt[0];
+			txt = txt[1];
+		}
+		console.log('TTS: "' + txt + '"');
+		switch(lg) {
+			case 'en':
+				var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsEn.sh', txt]);
+				break;
+			case 'es':
+				var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsEs.sh', txt]);
+				break;
+			case 'it':
+				var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsIt.sh', txt]);
+				break;
+			case 'de':
+				var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsDe.sh', txt]);
+				break;
+			default:
+				var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsFr.sh', txt]);
+		}
+		//if(txt === 'undefined') txt = '';
+		var blinkTime = (txt.length/15) + 1;
+		//console.log('blinkTime : ' + blinkTime);
+		leds.blinkEye((Math.floor(Math.random()*5) + 1)*50, blinkTime);
+	} else {
+		console.error('No network, can\'t get TTS data /!\\');
 	}
-	console.log('TTS: "' + txt + '"');
-	switch(lg) {
-		case 'en':
-			var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsEn.sh', txt]);
-			break;
-		case 'es':
-			var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsEs.sh', txt]);
-			break;
-		case 'it':
-			var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsIt.sh', txt]);
-			break;
-		case 'de':
-			var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsDe.sh', txt]);
-			break;
-		default:
-			var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/ttsFr.sh', txt]);
-	}
-	//if(txt === 'undefined') txt = '';
-	var blinkTime = (txt.length/15) + 1;
-	//console.log('blinkTime : ' + blinkTime);
-	leds.blinkEye((Math.floor(Math.random()*5) + 1)*50, blinkTime);
+});
 };
 exports.speak = speak;
 
