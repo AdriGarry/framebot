4cfdf066f3d1afbe0b1a697a8a065bd720d84523
Adri <adrigarry@gmail.com>
2016-04-25 20:30:42 +0200

    3.1.6 Fix clock

--- pgm/lib/clock.js
+++ pgm/lib/clock.js
@@ -14,54 +14,78 @@ var date = new Date();
 var hour = date.getHours();
 var pastHour = hour;
 
-var days;
+var clockPattern;
 var startClock = function(modeInit){
 	if(!modeInit){
 		console.log('Starting clock in quiet mode     -.-');
-		days = '1-5';
+		// clockPattern = '8-23 * * 1-5';
+		// clockPatternWE = '12-23 * * 0,7';
+		new CronJob('0 0 8-23 * * 1-5', function(){
+			// console.log('ringHour');
+			ringHour();
+		}, null, true, 'Europe/Paris');
+		new CronJob('0 30 8-23 * * 1-5', function(){
+			// console.log('ringHalfHour');
+			ringHalfHour();
+		}, null, true, 'Europe/Paris');
+		new CronJob('0 0 12-23 * * 0,7', function(){
+			// console.log('ringHour');
+			ringHour();
+		}, null, true, 'Europe/Paris');
+		new CronJob('0 30 12-23 * * 0,7', function(){
+			// console.log('ringHalfHour');
+			ringHalfHour();
+		}, null, true, 'Europe/Paris');
 	}else{
 		console.log('Starting clock in normal mode');
-		days = '0-7';
+		new CronJob('0 0 * * * *', function(){
+			// console.log('ringHour');
+			ringHour();
+		}, null, true, 'Europe/Paris');
+		new CronJob('0 30 * * * *', function(){
+			// console.log('ringHalfHour');
+			ringHalfHour();
+		}, null, true, 'Europe/Paris');
 	}
-	// console.log('days:' + days);
-	new CronJob('0 0 * * * ' + days, function(){
-		date = new Date();
-		hour = date.getHours();
-		console.log('It\'s ' + hour + ' o\'clock');
-		utils.testConnexion(function(connexion){
-			if(connexion == true){
-				tts.speak('fr', 'Il est ' + hour + ' heures');
-			}else{
-				if(cpHour > 12){
-					cpHour = hour - 12;
-				} else if(cpHour == 0){
-					cpHour = 12;
-				}
-				var oClock = setInterval(function(){
-					console.log('RING BELL ' + cpHour);
-					var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/clock.sh']);
-					cpHour--;
-					if(cpHour < 1){clearInterval(oClock);}
-				}, 1100);
-			}
-		});
-	}, null, true, 'Europe/Paris');
-	
-	new CronJob('0 34 * * * ' + days, function(){
-		date = new Date();
-		hour = date.getHours();
-		console.log('It\'s ' + hour + ' and a half');
-		utils.testConnexion(function(connexion){
-			if(connexion == true){
-				// if(cpHour > 12){cpHour = hour - 12};
-				tts.speak('fr', 'Il est ' + hour + ' heures 30');
-			}else{
-				var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/clock.sh', 'half']);
-			}
-		});
-	}, null, true, 'Europe/Paris');
 };
 exports.startClock = startClock;
+
+var ringHour = function(){
+	date = new Date();
+	hour = date.getHours();
+	console.log('It\'s ' + hour + ' o\'clock');
+	utils.testConnexion(function(connexion){
+		if(connexion == true){
+			tts.speak('fr', 'Il est ' + hour + ' heures');
+		}else{
+			if(cpHour > 12){
+				cpHour = hour - 12;
+			} else if(cpHour == 0){
+				cpHour = 12;
+			}
+			var oClock = setInterval(function(){
+				console.log('RING BELL ' + cpHour);
+				var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/clock.sh']);
+				cpHour--;
+				if(cpHour < 1){clearInterval(oClock);}
+			}, 1100);
+		}
+	});
+};
+
+var ringHalfHour = function(){
+	date = new Date();
+	hour = date.getHours();
+	console.log('It\'s ' + hour + ' and a half');
+	utils.testConnexion(function(connexion){
+		if(connexion == true){
+			// if(cpHour > 12){cpHour = hour - 12};
+			tts.speak('fr', 'Il est ' + hour + ' heures 30');
+		}else{
+			var deploy = spawn('sh', ['/home/pi/odi/pgm/sh/clock.sh', 'half']);
+		}
+	});
+};
 
 var setAlarms = function(){
 	console.log('Alarms On');
