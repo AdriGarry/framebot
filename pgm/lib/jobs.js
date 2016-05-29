2f88330f6aeae2e24f28a9407dd2b30e5b257eb5
Adri <adrigarry@gmail.com>
2016-05-29 14:54:46 +0200

    3.2.1

--- pgm/lib/jobs.js
+++ pgm/lib/jobs.js
@@ -153,7 +153,7 @@ var setAlarms = function(){
 	}, null, true, 'Europe/Paris');
 
 	new CronJob('13 15,45 17-22 * * *', function(){
-		tts.conversation(''); // Conversations aleatoires dans la journee
+		tts.conversation('RANDOM'); // Conversations aleatoires dans la journee
 	}, null, true, 'Europe/Paris'); // Signal des 1/4 d'heure, entre 17h et 23h
 
 	new CronJob('0 12 12 * * *', function() {
