73d7e87ecfc83e4900f924229af329bbe8d4554f
Adri <adrigarry@gmail.com>
2016-09-22 05:33:38 +0200

    3.5

--- pgm/web/js/config.js
+++ pgm/web/js/config.js
@@ -1,3 +1,4 @@
+'use strict'
 var app = angular.module('odiUI', ['ngMaterial']);
 
 app.controller('LogController', function ($scope, $timeout, $mdSidenav, $log) {
--- pgm/web/js/controller.js
+++ pgm/web/js/controller.js
@@ -1,7 +1,6 @@
-
+'use strict'
 app.controller('UIController', function($scope, $timeout, $mdSidenav){
-	$scope.message = 'Hello from UIController 2';
-
+	$scope.message = 'UIController V3';
 
 	$scope.showLogs = showLogs();
 	function showLogs(){
@@ -18,6 +17,89 @@ app.controller('UIController', function($scope, $timeout, $mdSidenav){
 			console.log('hideLogs()');
 		});
 	};
+
+
+	$scope.tuiles = {
+			mode: {
+				lib: 'Mode',
+				value: 'Ready',
+				color: 'blue',
+				rowspan : 1,
+				colspan: 1
+			}, switch: {
+				lib: 'Switch',
+				value: 1,
+				color: 'blueGrey',
+				rowspan : 1,
+				colspan: 1
+			}, volume: {
+				lib: 'Volume',
+				value: 'Normal',
+				color: 'lime',
+				rowspan : 1,
+				colspan: 1
+			}, tts: {
+				lib: 'TTS - Exclamation',
+				value: '<i>Soon available</i>',
+				color: 'cyan',
+				rowspan : 1,
+				colspan: 2
+			}, jukebox: {
+				lib: 'Jukebox & Radio',
+				value: '<i>Soon available</i>',
+				color: 'teal',
+				rowspan : 1,
+				colspan: 1
+			}, voiceMail: {
+				lib: 'VoiceMail',
+				value: 0,
+				color: 'indigo',
+				rowspan : 1,
+				colspan: 1
+			}, dateTime: {
+				lib: 'Date & Time',
+				value: '<i class="fa fa-3x fa-calendar"></i>&nbsp;&nbsp;&nbsp;<i class="fa fa-3x fa-clock-o"></i><br><i>Soon available</i>',
+				color: 'blue',
+				rowspan: 1,
+				colspan: 2
+			}, timer: {
+				lib: 'timer',
+				value: '<i class="fa fa-3x fa-hourglass"></i>',
+				color: 'blue',
+				rowspan: 1,
+				colspan: 1
+			}, cpuUsage: {
+				lib: 'CPU usage',
+				value: 2,
+				color: 'blueGrey',
+				rowspan : 1,
+				colspan: 1
+			}, cpuTemp: {
+				lib: 'CPU temperature',
+				value: 38,
+				color: 'lime',
+				rowspan : 1,
+				colspan: 1
+			}, alarms: {
+				lib: 'Alarms',
+				value: '<i>Soon available</i>',
+				color: 'orange',
+				rowspan : 1,
+				colspan: 1
+			}, about: {
+				lib: 'About',
+				value: 'Hi,<br>I\'m Odi the robot !',
+				color: 'indigo',
+				rowspan : 1,
+				colspan: 1
+			}, system: {
+				lib: 'System',
+				value: '<i>Soon available</i>',
+				color: 'lime',
+				rowspan : 1,
+				colspan: 1
+			}
+		};
 
 
 });
