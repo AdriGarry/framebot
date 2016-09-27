699d0eb2686ba34ccbf61b458e9605a6f59d6f46
Adri <adrigarry@gmail.com>
2016-09-27 04:56:37 +0200

    3.5

--- pgm/web/js/controller.js
+++ pgm/web/js/controller.js
@@ -1,5 +1,6 @@
 'use strict'
-app.controller('UIController', function($scope, $timeout, $sce, $mdSidenav, $mdBottomSheet, $mdToast, UIService, Tile){
+app.controller('UIController', function($rootScope, $scope, $timeout, $sce, $mdSidenav,
+		$mdBottomSheet, $mdToast, UIService, DashboardService){
 	$scope.loading = false;/*true*/
 	$scope.admin = false;
 
@@ -7,17 +8,16 @@ app.controller('UIController', function($scope, $timeout, $sce, $mdSidenav, $mdB
 	$scope.showLogs = showLogs();
 
 
-	/*$scope.object = new Tile();
-	console.log($scope.object);*/
-	$scope.tile = new Tile(1, 'TileTitle', 'orange', 1, 2, 1, [{label: 'RESTART', icon: 'bolt', url: 'url1'},{label: 'REFRESH', icon: 'refresh', url:'url2'}]);
-	console.log($scope.tile);
+	//$scope.tile = new Tile(1, 'TileTitle', 'orange', 1, 2, 1, [{label: 'RESTART', icon: 'bolt', url: 'url1'},{label: 'REFRESH', icon: 'refresh', url:'url2'}]);
+	/*console.log($scope.tile);*/
 
-	/** Function to pop down toast */
-	$scope.showToast = function(label) {
-		$mdToast.show($mdToast.simple().textContent(label).position('top right').hideDelay(1500));
+	$scope.dashboard = {
+		mode: '',
+		loading: false,
+		tileList: DashboardService.initTiles
 	};
 
-	$scope.ttsTuile = {
+	$scope.ttsTile = {
 		lib: 'TTS - Voice synthesizing',
 		value: '...',
 		color: 'blue',
@@ -28,6 +28,10 @@ app.controller('UIController', function($scope, $timeout, $sce, $mdSidenav, $mdB
 		voice: ':3'
 	};
 
+	/** Function to pop down toast */
+	$scope.showToast = function(label) {
+		$mdToast.show($mdToast.simple().textContent(label).position('top right').hideDelay(1500));
+	};
 
 	/** Function to expand Tile */
 	$scope.expandTile = function(obj){
@@ -80,22 +84,28 @@ app.controller('UIController', function($scope, $timeout, $sce, $mdSidenav, $mdB
 
 	/** Function to inject HTML code */
 	$scope.toHtml = function(html){
+		// console.log(html);
 		return $sce.trustAsHtml(html);
 	};
 
-	$scope.openBottomSheet = function(){
+	$scope.openBottomSheet = function(bottomSheetList){
 		if($scope.admin){
+			$rootScope.bottomSheetButtonList = bottomSheetList;
 			$scope.alert = '';
 			$mdBottomSheet.show({
 				templateUrl: 'content/bottom-sheet.html',
-				controller: 'BottomSheetCtrl',
+				controller: 'UIController',
 				clickOutsideToClose: true
-			}).then(function(clickedItem){
-				$scope.showToast(clickedItem + ' clicked!');
+			}).then(function(action){
+				$scope.showToast(action.label);
 			});
 		}
 	};
 
+	$scope.bottomSheetBtnClick = function(item){
+		console.log(item);
+		$mdBottomSheet.hide(item);
+	};
 
 	/*$timeout(function(){
 		$scope.loading = false;
@@ -103,19 +113,17 @@ app.controller('UIController', function($scope, $timeout, $sce, $mdSidenav, $mdB
 });
 
 
-app.controller('BottomSheetCtrl', function($scope, $mdBottomSheet){
-
+/*app.controller('BottomSheetCtrl', function($scope, $mdBottomSheet){
 	$scope.listItemClick = function(item){
 		console.log(item);
 		$mdBottomSheet.hide(item);
 	};
-
 	$scope.buttons = [
 		{label: 'Shutdown Odi', icon: 'power-off', url: ''},
 		{label: 'Sleep Odi', icon: 'moon-o', url: ''},
 		{label: 'Restart Odi', icon: 'bolt', url: ''}
 	];
-});
+});*/
 
 
 /* UIFactory FACTORY // SERVICE ??? */
@@ -159,3 +167,40 @@ app.factory('UIService', ['$http', function($http){
 
 	return UIService;
 }]);
+
+/* Dashboard Factory FACTORY // SERVICE ??? */
+app.factory('DashboardService', function(Tile){
+	var VALUE = 'value', ICON = 'icon', CUSTOM = 'custom';
+	var DashboardService = {};
+
+	/** Function to initialize dashboard */
+	var value = 'VALUE';
+	DashboardService.initTiles = { //Tile(id, label, color, rowspan, colspan, viewMode, value, actionList)
+		mode: new Tile(1, 'Mode', 'teal', 1, 1, VALUE, 'Ready', [{label: 'Sleep', icon: 'moon-o', url: 'URL1'},{label: 'Restart', icon: 'bolt', url: 'URL2'}]),
+		switch: new Tile(2, 'Switch', 'blueGrey', 1, 1, CUSTOM, []).bindHTML('switch'),
+		volume: new Tile(3, 'Volume', 'cyan', 1, 1, CUSTOM, 'Normal', [{label: '', icon: '', url: ''}]),
+		voicemail: new Tile(4, 'Voicemail', 'indigo', 1, 1, CUSTOM, '1', [{label: 'Empty voicemail', icon: 'trash-o', url: 'URL1'},{label: 'Listen messages', icon: 'voicemail', url: 'URL1'}]),
+		exclamation: new Tile(5, 'Exclamation', 'lime', 1, 1, ICON, 'commenting-o', [{label: 'Conversation', icon: 'comments-o', url: ''},{label: 'TTS', icon: 'commenting-o', url: ''},{label: 'Exclamation', icon: 'bullhorn', url: ''}]),
+		jukebox: new Tile(6, 'Jukebox', 'teal', 1, 1, CUSTOM, 'music', [{label: 'Jukebox', icon: 'random', url: ''},{label: 'FIP Radio', icon: 'globe', url: ''}]),
+		date: new Tile(7, 'Date', 'blue', 1, 1, ICON, 'calendar', [{label: '', icon: '', url: ''}]),
+		time: new Tile(8, 'Time', 'blue', 1, 1, ICON, 'clock-o', [{label: '', icon: '', url: ''}]),
+		timer: new Tile(9, 'Timer', 'orange', 1, 1, CUSTOM, 'hourglass-hald', [{label: 'Stop timer', icon: 'stop', url: ''},{label: 'Increment 1', icon: 'plus', url: ''}]),
+		cpu: new Tile(10, 'CPU', 'lime', 1, 1, CUSTOM, value, [{label: '', icon: '', url: ''}]),
+		weather: new Tile(11, 'Weather', 'teal', 1, 1, ICON, 'cloud', [{label: '', icon: '', url: ''}]),
+		alarms: new Tile(12, 'Alarms', 'indigo', 1, 1, CUSTOM, 'bell-o', [{label: '', icon: '', url: ''}]),
+		party: new Tile(13, 'Party', 'indigo', 1, 1, ICON, 'glass', [{label: '', icon: '', url: ''}]),
+		russia: new Tile(14, 'Russia', 'orange', 1, 1, ICON, 'star', [{label: 'Subway/Street', icon: '', url: ''},{label: 'Hymn', icon: '', url: ''}]),
+		test: new Tile(15, 'Test', 'blue', 1, 1, ICON, 'flag-checkered', [{label: '', icon: '', url: ''}]),
+		requestHistory: new Tile(16, 'Request History', 'blueGrey', 1, 1, ICON, 'file-text-o', [{label: '???', icon: 'file-text-o', url: ''},{label: 'Request History', icon: 'file-text-o', url: ''}]),
+		cigales: new Tile(17, 'Cigales', 'lime', 1, 1, ICON, 'bug', [{label: '', icon: '', url: ''}]),
+		system: new Tile(18, 'System', 'lightGreen', 1, 1, ICON, 'power-off', [{label: 'Shutdown Odi', icon: 'power-off', url: ''},{label: 'Reboot Odi', icon: 'refresh', url: ''}]),
+	};
+
+	/** Function to refresh dashboard */
+	// DashboardService.refreshDashboard = function(data){
+	// 	console.log('refreshDashboard()');
+	// 	DashboardService.initTiles
+	// };
+
+	return DashboardService;
+});
--- pgm/web/js/objects.js
+++ pgm/web/js/objects.js
@@ -1,13 +1,13 @@
+'use strict'
 /** Objects **/
-
 
 //app.value("trim", jQuery.trim);
 
 /** Tile object **/
 app.factory('Tile', function(){
 	// Define the constructor function.
-	function Tile(id, label, color, rowspan, colspan, value, bottomSheetList){
-		var self = this;
+	function Tile(id, label, color, rowspan, colspan, viewMode, value, actionList){
+		// var self = this;
 		// Basic attributes
 		this.id = id || '';
 		this.label = label || '';
@@ -17,26 +17,54 @@ app.factory('Tile', function(){
 
 		// Info attributes
 		this.value = value;
-		/*this.bottomSheetList = [];
-		bottomSheetList.forEach(function(bottomSheet){
-			console.log(bottomSheet);
-			self.bottomSheetList.push(bottomSheet);
-		});*/
-		this.bottomSheetList = bottomSheetList;
+		this.viewMode = viewMode; // 'icon' || 'value' || 'custom'
+		this.html = '';
+
+		// Action attributes
+		this.action = '';
+		this.actionList = actionList;
 	}
 
 	// Define the "instance" methods using the prototype and standard prototypal inheritance.
 	Tile.prototype = {
-		// getFirstName: function(){
-		// 	return(this.firstName);
-		// },
 		// Action attributes
-		onclick: function(){
+		click: function(){
+			console.log('click()');
+			// console.log(this);
+			if(this.actionList && this.actionList.constructor === Array && this.actionList.length > 0){
+				console.log('IF()');
+				//openBottomSheet
+			}else{
+				console.log('ELSE()');
+			}
+		},
+		bindHTML: function(element){
+			// console.log('bindHTML()');
+			console.log(element);
+			var html = '';
+			switch(element){
+				case 'switch':
+					html = '<md-switch class="switch" ng-disabled="true" title="Switch position" aria-label="Switch position" ng-model="disabledModel"></md-switch>';
+				break;
+				case 'volume':
+					html = '';
+				break;
+				default:
+					html = '';
+				break;
+			}
+			console.log(html);
+			this.html = html;
+			console.log(this);
+			//return html;
+		}
+
+		/*onclick: function(){
 		},
 		onHold: function(){
 		},
 		onDBclick: function(){
-		}
+		}*/
 	};
 
 	// Return constructor
