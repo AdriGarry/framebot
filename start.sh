50c84657cecebf85ff4ab3ec8e6e4732373d689c
Adri <adrigarry@gmail.com>
2016-10-14 23:22:03 +0200

    3.5 create /tmp directory if no exists

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ echo "start.sh -> Start Odi Pgm..."
 # sudo node /home/pi/odi/pgm/main.js >> /home/pi/odi/log/odi2.log 2>&1 &
 
 # TEST IF TMP DIRECTORY EXISTS
+if [ ! -d "$DIRECTORY" ]; then
+	mkdir /home/pi/odi/pgm/tmp
+fi
 
 sudo node /home/pi/odi/pgm/manager.js 2>&1 | sudo tee -a /home/pi/odi/log/odi.log &
 # sudo node /home/pi/odi/pgm/main.js 2>&1 | tee -a /home/pi/odi/log/odi2.log &
