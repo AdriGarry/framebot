5060f5c46fbbbb7ed1cf448d82cf21249f85f691
Adrien <adrigarry@gmail.com>
2016-11-12 13:18:28 +0100

    3.7

--- core/controllers/jobs.js
+++ core/controllers/jobs.js
@@ -126,7 +126,7 @@ function setAlarms(){
 		tts.speak({lg:'fr', voice: 'espeak', msg:'go go go, allez au boulot'});
 	}, null, true, 'Europe/Paris');
 
-	new CronJob('0 30 18 * * 1-5', function() {
+	new CronJob('0 30 18 * * 1-5', function(){
 		utils.testConnexion(function(connexion){
 			setTimeout(function(){
 				if(connexion == true){
@@ -139,12 +139,12 @@ function setAlarms(){
 	}, null, true, 'Europe/Paris');
 
 	// WEEKEND
-	new CronJob('0 45,55 11 * * 0,6', function() {
+	new CronJob('0 45,55 11 * * 0,6', function(){
 		console.log('Morning Sea... Let\'s start the day with some waves !');
 		spawn('sh', ['/home/pi/odi/core/sh/sounds.sh', 'MorningSea']);
 	}, null, true, 'Europe/Paris');
 
-	new CronJob('0 0 12 * * 0,6', function() {
+	new CronJob('0 0 12 * * 0,6', function(){
 		console.log('COCORICO !!');
 		var deploy = spawn('sh', ['/home/pi/odi/core/sh/sounds.sh', 'cocorico']);
 		utils.testConnexion(function(connexion){
@@ -165,9 +165,9 @@ function setAlarms(){
 	}, null, true, 'Europe/Paris');
 
 	// ALL DAYS
-	new CronJob('0 1 13 * * *', function() {
+	new CronJob('0 1 13 * * *', function(){
 		console.log('Il est 13 heures et tout va bien !');
-		var deploy = spawn('sh', ['/home/pi/odi/core/sh/sounds.sh', '13Heures']);
+		spawn('sh', ['/home/pi/odi/core/sh/sounds.sh', '13Heures']);//var deploy = 
 	}, null, true, 'Europe/Paris');
 
 	new CronJob('13 15,45 17-22 * * *', function(){
@@ -194,7 +194,7 @@ function setAutoLifeCycle(param){
 			console.log('AutoLifeCycle go to sleep !');
 			hardware.restartOdi(255);
 		}, null, true, 'Europe/Paris');
-		new CronJob('3 0 2 * * 0,6', function() {
+		new CronJob('3 0 2 * * 0,6', function(){
 			console.log('AutoLifeCycle go to sleep !');
 			hardware.restartOdi(255);
 		}, null, true, 'Europe/Paris');
@@ -205,21 +205,21 @@ function setAutoLifeCycle(param){
 function setBackgroundJobs(){
 	console.log('Background jobs initialised');
 
-	new CronJob('13 13 13 * * 1-6', function() {
+	new CronJob('13 13 13 * * 1-6', function(){
 		tts.speak({voice:'espeak', lg:'en', msg:'Auto restart'}); // Daily restart Odi's core
 		setTimeout(function(){
 			hardware.restartOdi();
 		}, 3000);
 	}, null, true, 'Europe/Paris');
 
-	new CronJob('13 13 13 * * 0', function() {
+	new CronJob('13 13 13 * * 0', function(){
 		tts.speak({voice:'espeak', lg:'fr', msg:'Auto reboot'}); // Weekly RPI reboot
 		setTimeout(function(){
 			hardware.reboot();
 		}, 3000);
 	}, null, true, 'Europe/Paris');
 
-	new CronJob('0 0 5 * * 2', function() {
+	new CronJob('0 0 5 * * 2', function(){
 		console.log('Clean log files  /!\\'); // Weekly cleaning of logs
 		hardware.cleanLog();
 	}, null, true, 'Europe/Paris');
