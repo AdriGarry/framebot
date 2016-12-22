7ee77e6384fae5e6aa1eb0e219b5132c280f9ac5
Adrien <adrigarry@gmail.com>
2016-12-22 21:56:02 +0100

    3.8 components

--- web/js/components/tts.js
+++ web/js/components/tts.js
@@ -6,5 +6,16 @@ app.component('tts', {
 	templateUrl: '/js/components/tts.html',
 	controller: function(){
 		console.log('TTS_ this.tile', this.tile);
+		/** Function to expand Tile */
+		this.expandTile = function(obj){
+			if(obj.hasOwnProperty('rowspan')) obj.rowspan = 2;
+		};
+		/** Function to reduce Tile */
+		this.reduceTile = function(obj){
+			console.log(obj);
+			obj.rowspan = 1;
+			console.log(obj);
+		};
+
 	}
 });
