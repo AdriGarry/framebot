7bd94ead9bffc6cc1447933767609702ed3b90de
Adrien <adrigarry@gmail.com>
2017-01-28 07:15:19 +0100

    3.8 components !!

--- web/js/components/tts.js
+++ web/js/components/tts.js
@@ -1,13 +1,13 @@
 /** mode component */
-app.component('mode', {
+app.component('tts', {
 	bindings: {
 		data: '<', // TODO revoir le binding : unidirectionnel?
 	},
-	templateUrl: '/js/components/mode.html',
+	templateUrl: '/js/components/tts.html',
 	controller: function(DefaultTile){
 		//function Tile(id, label, color, rowspan, colspan, viewMode, value, actionList){
 		var tileParams = {
-			label: 'Mode',
+			label: 'TTS - Voice synthesizing',
 			actionList:[{label: 'Reset', icon: 'retweet', url: '/resetConfig'},{
 				label: '!Debug', icon: 'terminal', url: '/toggleDebug'},{
 				label: 'Sleep', icon: 'moon-o', url: '/sleep'},{
@@ -15,8 +15,14 @@ app.component('mode', {
 		};
 
 		//this.tileData = this.tile; console.log('this.tileData', this.tileData);
+		//this.tile = new DefaultTile(tileParams); console.log('this.tile', this.tile);
+
+		//this.tileData = this.tile; console.log('this.tileData', this.tileData);
 		this.tile = new DefaultTile(tileParams); //console.log('this.tile', this.tile);
 		this.tile.data = this.data; console.log('this.tile', this.tile);
+
+		// $scope.tile = new DefaultTile(1, 'Mode', 'teal', 1, 1, 'custom', '-',
+		// 	[{label: 'Reset', icon: 'retweet', url: '/resetConfig'},{label: '!Debug', icon: 'terminal', url: '/toggleDebug'},{label: 'Sleep', icon: 'moon-o', url: '/sleep'},{label: 'Restart', icon: 'bolt', url: '/odi'}]);
 
 
 		/** Overwrite tile action */
--- web/js/components/volume.js
+++ web/js/components/volume.js
@@ -1,22 +1,23 @@
 /** mode component */
-app.component('mode', {
+app.component('volume', {
 	bindings: {
 		data: '<', // TODO revoir le binding : unidirectionnel?
 	},
-	templateUrl: '/js/components/mode.html',
+	templateUrl: '/js/components/volume.html',
 	controller: function(DefaultTile){
 		//function Tile(id, label, color, rowspan, colspan, viewMode, value, actionList){
 		var tileParams = {
-			label: 'Mode',
-			actionList:[{label: 'Reset', icon: 'retweet', url: '/resetConfig'},{
-				label: '!Debug', icon: 'terminal', url: '/toggleDebug'},{
-				label: 'Sleep', icon: 'moon-o', url: '/sleep'},{
-				label: 'Restart', icon: 'bolt', url: '/odi'}]
+			id: 1,
+			label: 'Volume',
+			actionList:[{label: 'Mute', icon: 'retweet', url: '/mute'}]
 		};
 
 		//this.tileData = this.tile; console.log('this.tileData', this.tileData);
 		this.tile = new DefaultTile(tileParams); //console.log('this.tile', this.tile);
 		this.tile.data = this.data; console.log('this.tile', this.tile);
+
+		// $scope.tile = new DefaultTile(1, 'Mode', 'teal', 1, 1, 'custom', '-',
+		// 	[{label: 'Reset', icon: 'retweet', url: '/resetConfig'},{label: '!Debug', icon: 'terminal', url: '/toggleDebug'},{label: 'Sleep', icon: 'moon-o', url: '/sleep'},{label: 'Restart', icon: 'bolt', url: '/odi'}]);
 
 
 		/** Overwrite tile action */
