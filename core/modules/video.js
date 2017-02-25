91701d447944b645c0fd75823fca9ccd53614998
Adrien <adrigarry@gmail.com>
2017-02-25 22:48:41 +0100

    4.0 video random cycle 30 min

--- core/modules/video.js
+++ core/modules/video.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Module Service
+// Module Video
 
 var spawn = require('child_process').spawn;
 var tts = require(CORE_PATH + 'modules/tts.js');
@@ -10,29 +10,23 @@ module.exports = {
 	sleep: sleep
 };
 
-/** Function to launch a video cycle for 1 hour */
+/** Function to launch a video cycle for 30 minutes */
 function start(){
 	spawn('sh', ['/home/pi/odi/core/sh/video.sh', 'random']);
 	console.log('videoCycle() for one hour');
 	setTimeout(function(){
 		sleep();
 	}, 40*1000);
-	// }, 60*60*1000);
+	// }, 30*60*1000);
 };
 
 /** Function to sleep monitor */
 function sleep(mode){
-	// console.log('CONFIG', CONFIG);
-	// console.log('===>mode', mode);
-	// console.log('video.sleep()');
 	console.log('video.sleep(mode)', mode);
 	if(isNaN(mode)){
-		// console.log('DANS LE TEST');
-		tts.speak({voice: 'espeak', lg:'en', msg: 'stopping video output'});
+		tts.speak({voice: 'espeak', lg:'fr', msg: 'Fin du cycle video'});
+		// tts.speak({voice: 'espeak', lg:'en', msg: 'stopping video output'});
 	}
-	// if(CONFIG.mode !== 'sleep') tts.speak({voice: 'espeak', lg:'en', msg: 'stopping video output'});
 	spawn('sh', ['/home/pi/odi/core/sh/video.sh', 'sleep']);
-	// tts.speak({voice: 'espeak', lg:'fr', msg: 'Fin du cycle video'});
-	// spawn('sh', ['/home/pi/odi/core/sh/video.sh', 'sleep']);
 };
 
