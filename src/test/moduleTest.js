769d3bd3414646d759d186dc357ebdae55954bf3
Adrien <adrigarry@gmail.com>
2017-10-26 01:34:17 +0200

    random test tts

--- src/test/moduleTest.js
+++ src/test/moduleTest.js
@@ -9,14 +9,19 @@ const Rx = require('rxjs');
 
 var Flux = require(Odi.CORE_PATH + 'Flux.js');
 
-const testTTSList = ['Test', 'T', 'TT'];
+const testTTSList = [
+	{ lg: 'en', msg: 'Test' },
+	{ lg: 'fr', msg: 'Test' },
+	{ lg: 'en', msg: 'T' },
+	{ lg: 'fr', msg: 'T' }
+];
 
 module.exports.run = function(callback) {
 	Flux.next('module', 'led', 'toggle', { leds: ['eye', 'belly', 'satellite'], value: 1 });
+	Flux.next('module', 'led', 'toggle', { leds: ['eye', 'belly', 'satellite'], value: 0 }, 2);
+	Flux.next('module', 'led', 'blink', { leds: ['belly'], speed: 900, loop: 100 }, 2);
 
-	Flux.next('module', 'tts', 'speak', { lg: 'en', msg: testTTSList[Math.floor(Math.random() * testTTSList.length)] });
-
-	Flux.next('module', 'led', 'blink', { leds: ['belly'], speed: 900, loop: 10 });
+	Flux.next('module', 'tts', 'speak', testTTSList[Math.floor(Math.random() * testTTSList.length)]);
 
 	Flux.next('module', 'sound', 'mute', { delay: 7, message: 'DELAY 7' }, 3); //5
 	Flux.next('module', 'sound', 'mute', { delay: 13, message: 'DELAY 13' }, 3);
