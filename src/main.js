414984cce9f28c1439089d5d9220ee180512d55f
Adrien <adrigarry@gmail.com>
2017-11-04 05:30:50 +0100

    observers sequence depending on Odi.conf.mode

--- src/main.js
+++ src/main.js
@@ -20,52 +20,39 @@ var log = new (require(Odi.CORE_PATH + 'Logger.js'))(__filename, Odi.conf.debug)
 log.debug('argv', argv);
 
 var Utils = require(Odi.CORE_PATH + 'Utils.js');
-
 var Flux = require(Odi.CORE_PATH + 'Flux.js');
-
 var Brain = require(Odi.CORE_PATH + 'Brain.js');
 
-/*var modules = {
-	led: require(Odi.CORE_PATH + 'modules/led.js'),
-	hardware: require(Odi.CORE_PATH + 'modules/hardware.js'),
-	sound: Odi.conf.mode == 'sleep' ? null : require(Odi.CORE_PATH + 'modules/sound.js'),
-	tts: Odi.conf.mode == 'sleep' ? null : require(Odi.CORE_PATH + 'modules/tts.js')
-};
-log.info('Modules loaded:', Object.keys(modules).join(', '));
-var controllers = {
-	button: require(Odi.CORE_PATH + 'controllers/button.js'),
-	jobs: require(Odi.CORE_PATH + 'controllers/jobs.js')
-	// server: require(Odi.CORE_PATH + "controllers/server.js")
-};
-log.info('Controllers loaded:', Object.keys(controllers).join(', '));
-var services = {
-	max: require(Odi.CORE_PATH + 'services/max.js'), // Max & Co...
-	mood: require(Odi.CORE_PATH + 'services/mood.js'), // random, exclamation, badBoy, party, [cigales ?]
-	music: require(Odi.CORE_PATH + 'services/music.js'), // fip, jukebox
-	system: require(Odi.CORE_PATH + 'services/system.js'),
-	time: require(Odi.CORE_PATH + 'services/time.js'),
-	tools: require(Odi.CORE_PATH + 'services/tools.js'), // ??
-	video: require(Odi.CORE_PATH + 'services/video.js'),
-	voiceMail: require(Odi.CORE_PATH + 'services/voicemail.js')
-};
-log.info('Services loaded:', Object.keys(services).join(', '));*/
-
-const modules = ['modules', 'led','hardware', 'sound', 'tts'];
-const controllers = ['controllers','button', 'jobs'/*, 'server' */];
-const services = ['services', 'system', 'time', 'tools', 'music', 'mood', 'voicemail', 'max', 'video'];
-
-launchObservers(modules);
-launchObservers(controllers);
-launchObservers(services);
-function launchObservers(oberservers){
-	let type = oberservers.shift();
-	for(let i = 0;i<oberservers.length;i++){
-		require(Odi.CORE_PATH + type + '/' + oberservers[i] + '.js')
-		// log.info(oberservers[i], 'observer loaded');
+const observers = {
+	modules: {
+		sleep: ['led','hardware'],
+		all: ['sound', 'tts']
+	},
+	controllers: {
+		sleep: ['button', 'jobs'/*, 'server' */],
+		all: []
+	},
+	services: {
+		sleep: ['system', 'tools', 'voicemail'],
+		all: ['time', 'music', 'mood', 'max', 'video']
 	}
-	log.info(type, 'loaded:', oberservers.join(', '));
-}
+};
 
+Object.keys(observers).forEach(function(observer) {
+	let observersLoaded = '';
+	for(let i = 0;i<observers[observer].sleep.length;i++){
+		require(Odi.CORE_PATH + observer + '/' + observers[observer].sleep[i] + '.js');
+	}
+	observersLoaded += observers[observer].sleep.join(', ');
+	if(Odi.conf.mode != 'sleep'){
+		for(let i = 0;i<observers[observer].all.length;i++){
+			require(Odi.CORE_PATH + observer + '/' + observers[observer].all[i] + '.js');
+		}
+		observersLoaded += ', '+observers[observer].all.join(', ');
+	}
+
+	log.info(observer, 'loaded:', observersLoaded);
+});
 
 log.info('Odi ready in' + Utils.getExecutionTime(startTime, '     ') + 'ms');
 
@@ -81,19 +68,8 @@ if (test || Odi.conf.mode == 'test') {
 	}, 500);
 }
 
-if(Odi.conf.mode == 'sleep') {
-	// console.log('1111');
-	Flux.next('service', 'system', 'restart', null, 10);
-}else{
-	// console.log('2222');
-	Flux.next('service', 'system', 'restart', 'sleep', 10);
-}
-
-
-/*setTimeout(function() {
-	log.DEBUG('process.exit');
-	process.exit();
-}, 30000);*/
+if(Odi.conf.mode == 'sleep') Flux.next('service', 'system', 'restart', null, 10);
+else Flux.next('service', 'system', 'restart', 'sleep', 10);
 
 // var start = new Date();
 // setTimeout(function(argument) {
