d73b46767e7a8b2c3f44393ef1021cf1b45f5fee
Adrien <adrigarry@gmail.com>
2017-11-04 08:52:24 +0100

    456

---
+++ src/shell/soundsParty.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+#___Sons Party mode
+#sudo node /home/pi/odi/core/modules/allLedsOn.js
+sudo node /home/pi/odi/core/modules/leds.js allLedsOn
+volume=300
+if [ $1 = "startParty" ]
+then
+	sound="/home/pi/odi/media/mp3/party/startParty.mp3"
+	volume=600
+elif [ $1 = "pasAssezSaoul" ]
+then
+	sound="/home/pi/odi/media/mp3/party/pasAssezSaoul.mp3"
+	volume=600
+elif [ $1 = "puisJeExprimer" ]
+then
+	sound="/home/pi/odi/media/mp3/party/puisJeExprimer.mp3"
+	volume=600
+elif [ $1 = "discours" ]
+then
+	#sudo node /home/pi/odi/core/modules/allLedsOn.js
+	sudo node /home/pi/odi/core/modules/leds.js allLedsOn
+	sound="/home/pi/odi/media/mp3/party/discours.mp3"
+	volume=300
+elif [ $1 = "23h" ]
+then
+	sound="/home/pi/odi/media/mp3/party/23h.mp3"
+	volume=600
+elif [ $1 = "23h30" ]
+then
+	sound="/home/pi/odi/media/mp3/party/23h30.mp3"
+	volume=600
+elif [ $1 = "compteARebours" ]
+then
+	#sudo node /home/pi/odi/core/modules/allLedsOn.js
+	sudo node /home/pi/odi/core/modules/leds.js allLedsOn
+	sound="/home/pi/odi/media/mp3/party/compteARebours.mp3"
+	volume=600
+elif [ $1 = "compteARebours2" ]
+then
+	#sudo node /home/pi/odi/core/modules/allLedsOn.js
+	sudo node /home/pi/odi/core/modules/leds.js allLedsOn
+	sound="/home/pi/odi/media/mp3/party/compteARebours2.mp3"
+	volume=600
+else
+	sound="/home/pi/odi/media/mp3/sounds/circuitsTransistors.mp3"
+fi
+
+volume=800
+
+sudo omxplayer -o local --vol $volume $sound
+#sudo node /home/pi/odi/core/modules/allLedsOff.js
+sudo node /home/pi/odi/core/modules/leds.js allLedsOff
---
+++ src/shell/watchAction.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Watch Action
+
+#__Function to update last modified date & time of Odi's files
+updateLastModified(){
+	lastUpdate=`ls | egrep ".*\.watcher$"`
+	if [ -n "$lastUpdate" ]; then
+		sudo rm "/home/pi/odi/$lastUpdate"
+	fi
+	# echo "updateLastModified()_lastUpdate:"$lastUpdate
+	now=`date +"%Y-%m-%d %H:%M:%S"`
+	# now=`date +"%d-%m %H:%M"`
+	touch /home/pi/odi/"$now".watcher
+	echo "Last modified time updated:" $now
+}
+
+echo $1
+
+if [ $1 = "updateLastModified" ]; then
+	updateLastModified
+elif [ $1 = "watch" ]; then
+	sh "/home/pi/odi/core/sh/watcher.sh" /home/pi/odi/core/ "sh /home/pi/odi/core/sh/watchAction.sh updateLastModified" &
+	sh "/home/pi/odi/core/sh/watcher.sh" /home/pi/odi/data/ "sh /home/pi/odi/core/sh/watchAction.sh updateLastModified" &
+	sh "/home/pi/odi/core/sh/watcher.sh" /home/pi/odi/web/ "sh /home/pi/odi/core/sh/watchAction.sh updateLastModified" &
+	echo "--------------> TOTO2"
+fi
