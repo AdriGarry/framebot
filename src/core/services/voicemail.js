67099e3eb4fca6ecc12a04d00ed633d62e4523f4
Adrien <adrigarry@gmail.com>
2017-11-05 13:33:28 +0100

    voicemail ok!

--- src/core/services/voicemail.js
+++ src/core/services/voicemail.js
@@ -13,7 +13,6 @@ Flux.service.voicemail.subscribe({
 	next: flux => {
 		log.info('VoiceMail service', flux);
 		if(flux.id == 'new'){
-			console.log('TOTO44');
 			addVoiceMailMessage(flux.value);
 		}else if(flux.id == 'check'){
 			checkVoiceMail();
@@ -36,6 +35,9 @@ function addVoiceMailMessage(tts){
 	tts = JSON.stringify(tts);
 	Utils.appendJsonFile(VOICEMAIL_FILE, tts);
 	Utils.appendJsonFile(VOICEMAIL_FILE_HISTORY, tts);
+	setTimeout(function(){
+		updateVoicemailMessage();
+	}, 1000);
 };
 
 var clearVoiceMailDelay;
@@ -52,7 +54,7 @@ function checkVoiceMail(callback){
 			clearVoiceMailDelay = setTimeout(function(){ // Clearing VoiceMail
 				clearVoiceMail();
 			}, DELAY_TO_CLEAR_VOICEMAIL);
-			// console.log('VoiceMail will be cleared in 10 minutes.');
+			// log.info('VoiceMail will be cleared in 10 minutes.');
 			log.info('VoiceMail will be cleared in 6 hours.');
 			if(callback) callback(true); // for other action
 			//callback(true);
@@ -68,28 +70,24 @@ function checkVoiceMail(callback){
 /** Function voicemail flag (blink belly if any message) */
 (function voiceMailFlag(){
 	log.info('VoiceMail flag initialized');
-	var nbMessages;
+	updateVoicemailMessage();
 	setInterval(function(){
-		nbMessages = areThereAnyMessages();
-		if(nbMessages > 0){ // if(nbMessages)
-			Flux.next('module', 'led', 'blink', {leds: ['belly'], speed: 200, loop: 2}, null, null, true);
-		}
+		updateVoicemailMessage();
 	}, 10000);
 })();
 
 /** Function to return number of voicemail message(s) */
-function areThereAnyMessages(){
-	var nbMessages = 0;
+function updateVoicemailMessage(){
 	try{
 		var messages = fs.readFileSync(VOICEMAIL_FILE, 'UTF-8');
 		messages = JSON.parse(messages);
-		nbMessages = messages.length;
-		if(Odi.run.voicemail != nbMessages) Odi.run.voicemail = nbMessages;
+		Odi.run.voicemail = messages.length;
+		if(Odi.run.voicemail > 0){
+			Flux.next('module', 'led', 'blink', {leds: ['belly'], speed: 200, loop: 2}, null, null, true);
+		}
 	}catch(e){
-		nbMessages = 0;
+		Odi.run.voicemail = 0;
 	}
-	return nbMessages;
-	//console.debug('AreThereAnyMessages ? ' + (nbMessages > 0 ? 'YES, ' + nbMessages + ' messages !' : 'NO'));
 };
 
 /** Function to clear all voicemail messages */
@@ -99,6 +97,7 @@ function clearVoiceMail(){
 			if(err.code === 'ENOENT') log.info('clearVoiceMail : No message to delete !');
 			else Odi.error(err);
 		}else{
+			updateVoicemailMessage();
 			log.info('VoiceMail Cleared !');
 			Flux.next('module', 'tts', 'speak', {lg:'en', voice: 'google', msg:'VoiceMail Cleared'});
 		}
