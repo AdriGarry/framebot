3b0aa9c31ac457d4632f1ed58fce80dcc883d22f
Adrien Garry <adrigarry@gmail.com>
2018-06-07 14:40:04 +0200

    5.2.10 Odi.sh fix

--- odi.sh
+++ odi.sh
@@ -4,6 +4,14 @@ clear
 
 ODI_PATH="/home/pi/odi"
 echo $ODI_PATH
+
+# Test if log directory exists
+if [ ! -d "$ODI_PATH"/log ];
+then
+	mkdir "$ODI_PATH"/log
+	echo "log directory created"
+fi
+
 echo "odi.sh -> Starting Wrapper... [$*]" | sudo tee -a "$ODI_PATH/log/odi.log"
 
 # sudo python /home/pi/odi/core/py/buttons.py 2>&1 | sudo tee -a /home/pi/odi/log/odi.log &
