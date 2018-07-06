78f8f14fa91ec25bc61212f743ed599e37c031fb
Adri <adrigarry@gmail.com>
2018-07-07 01:35:55 +0200

    5.5.0 log fix

--- src/core/controllers/server/middleware.js
+++ src/core/controllers/server/middleware.js
@@ -30,7 +30,7 @@ var securityMiddleware = function(req, res, next) {
 		if (req.isSocket) {
 			log.INFO('..............This is a socket (search: "req.isSocket")!!');
 		} else {
-			log.INFO('..............This NOT is a socket (search: "req.isSocket")!!');
+			log.INFO('..............This is NOT a socket (search: "req.isSocket")!!');
 		}
 		log.error('Incoming socket /!\\ /!\\');
 		log.info(req); // TODO revoir cette sécurité...
