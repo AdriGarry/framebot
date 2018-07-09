ae72a03ba535873a8341986713e951cf61b9bec1
Adri <adrigarry@gmail.com>
2018-07-10 01:01:50 +0200

    GEOLOCATION_SERVICE_URL

--- src/web/js/app.js
+++ src/web/js/app.js
@@ -13,7 +13,7 @@ app.constant('CONSTANTS', {
 		'((([0–9A-Fa-f]{1,4}:){7}[0–9A-Fa-f]{1,4})|(([0–9A-Fa-f]{1,4}:){6}:[0–9A-Fa-f]{1,4})|(([0–9A-Fa-f]{1,4}:){5}:([0–9A-Fa-f]{1,4}:)?[0–9A-Fa-f]{1,4})|(([0–9A-Fa-f]{1,4}:){4}:([0–9A-Fa-f]{1,4}:){0,2}[0–9A-Fa-f]{1,4})|(([0–9A-Fa-f]{1,4}:){3}:([0–9A-Fa-f]{1,4}:){0,3}[0–9A-Fa-f]{1,4})|(([0–9A-Fa-f]{1,4}:){2}:([0–9A-Fa-f]{1,4}:){0,4}[0–9A-Fa-f]{1,4})|(([0–9A-Fa-f]{1,4}:){6}((b((25[0–5])|(1d{2})|(2[0–4]d)|(d{1,2}))b).){3}(b((25[0–5])|(1d{2})|(2[0–4]d)|(d{1,2}))b))|(([0–9A-Fa-f]{1,4}:){0,5}:((b((25[0–5])|(1d{2})|(2[0–4]d)|(d{1,2}))b).){3}(b((25[0–5])|(1d{2})|(2[0–4]d)|(d{1,2}))b))|(::([0–9A-Fa-f]{1,4}:){0,5}((b((25[0–5])|(1d{2})|(2[0–4]d)|(d{1,2}))b).){3}(b((25[0–5])|(1d{2})|(2[0–4]d)|(d{1,2}))b))|([0–9A-Fa-f]{1,4}::([0–9A-Fa-f]{1,4}:){0,5}[0–9A-Fa-f]{1,4})|(::([0–9A-Fa-f]{1,4}:){0,6}[0–9A-Fa-f]{1,4})|(([0–9A-Fa-f]{1,4}:){1,7}:))',
 		'g'
 	), // IPv4 & IPv6 regex: https://sroze.io/regex-ip-v4-et-ipv6-6cc005cabe8c
-	IP_LOCALIZATOR_SERVICE_URL: 'http://www.traceip.net/?query=',
+	IP_LOCATION_SERVICE_URL: 'http://www.traceip.net/?query=',
 	GEOLOCATION_SERVICE_URL: 'http://www.gps-coordinates.org/my-location.php?lat=$lat&lng=$lng'
 });
 
@@ -48,7 +48,7 @@ app.filter('formatLog', function(CONSTANTS) {
 			if (ip.search(/(^192\.168\.)/g)) {
 				return (
 					'from [<a href="' +
-					CONSTANTS.IP_LOCALIZATOR_SERVICE_URL +
+					CONSTANTS.IP_LOCATION_SERVICE_URL +
 					ip +
 					'" title="Localize this IP" target="_blank">' +
 					ip +
