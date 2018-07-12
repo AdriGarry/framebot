e77a211e12871c32e787a6088fd401164e539076
Adri <adrigarry@gmail.com>
2018-07-12 20:40:08 +0200

    geolocation code update

--- src/web/js/services.js
+++ src/web/js/services.js
@@ -165,17 +165,17 @@ app.service('UIService', [
 			);
 		};
 
-		function maPosition(position) {
-			var infopos = 'Position déterminée :\n';
-			infopos += 'Latitude : ' + position.coords.latitude + '\n';
-			infopos += 'Longitude: ' + position.coords.longitude + '\n';
-			infopos += 'Altitude : ' + position.coords.altitude + '\n';
-			document.getElementById('infoposition').innerHTML = infopos;
-		}
+		// function maPosition(position) {
+		// 	var infopos = 'Position déterminée :\n';
+		// 	infopos += 'Latitude : ' + position.coords.latitude + '\n';
+		// 	infopos += 'Longitude: ' + position.coords.longitude + '\n';
+		// 	infopos += 'Altitude : ' + position.coords.altitude + '\n';
+		// 	document.getElementById('infoposition').innerHTML = infopos;
+		// }
 
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(maPosition);
-		}
+		// if (navigator.geolocation) {
+		// 	navigator.geolocation.getCurrentPosition(maPosition);
+		// }
 		navigator.geolocation.watchPosition(
 			function(position) {
 				console.log("i'm tracking you!", position);
