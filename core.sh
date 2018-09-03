cd3ea42a5bdf0da0912ad91c5711d699969d913c
Adrien Garry <agarry@enovacom.fr>
2018-09-03 13:55:18 +0200

    5.9 core.sh + wrapper.json

--- core.sh
+++ core.sh
@@ -8,6 +8,17 @@ _NAME = '$1'
 _PATH = '$pwd'
 echo $_PATH
 
+install(){
+	echo "installing core..."
+	sudo chmod +x "$_PATH"/core.sh
+}
+
+if [ $1 = "install" ]; then
+	install;;
+	exit 0;;
+fi
+
+
 # Test if log directory exists
 if [ ! -d "$_PATH"/log ];
 then
@@ -19,3 +30,10 @@ echo "core.sh -> Starting Wrapper... [$*]" | sudo tee -a "$_PATH/log/$_NAME.log"
 
 # sudo python /home/pi/odi/core/py/buttons.py 2>&1 | sudo tee -a /home/pi/odi/log/Odi.log &
 sudo node "$_PATH"/src/wrapper.js $* 2>&1 | sudo tee -a "$_PATH/log/$_NAME.log" &
+
+
+#___Fonction silence automatique dans une heure
+#if [ $1 = "auto" ]
+#then
+#	sudo omxplayer -o local --vol 200 /home/pi/odi/media/mp3/system/autoMute.mp3
+#fi
