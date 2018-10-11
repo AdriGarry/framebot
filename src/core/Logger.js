e8b869e381e347de2674890c60820044a2e4627d
Adri <adrigarry@gmail.com>
2018-10-12 00:29:12 +0200

    Logger WIP

--- src/core/Logger.js
+++ src/core/Logger.js
@@ -4,129 +4,78 @@
 module.exports = Logger;
 
 const util = require('util');
+const Utils = require(_PATH + 'src/core/Utils.js');
 
 const LEVEL = { INFO: 'info', DEBUG: 'debug', TRACE: 'trace' };
 const TIMEOUT = 15;
 const TIMESTAMP_PATTERN = { NORMAL: 'D/M h:m:s', SLEEP: 'D/M_h:m:s' };
 
-var Utils, Core, timestamp;
+var Core, timestamp;
 
 var logLevel = LEVEL.INFO;
 
-function Logger(filename, modeCore) {
-	Utils = require(_PATH + 'src/core/Utils.js');
-	if (modeCore && modeCore == 'sleep') {
-		timestamp = TIMESTAMP_PATTERN.SLEEP;
-	} else {
-		timestamp = TIMESTAMP_PATTERN.NORMAL;
+module.exports = class Logger {
+	constructor(filename, modeCore) {
+		if (modeCore && modeCore == 'sleep') {
+			timestamp = TIMESTAMP_PATTERN.SLEEP;
+		} else {
+			timestamp = TIMESTAMP_PATTERN.NORMAL;
+		}
+		filename = filename.match(/(\w*).js/g)[0];
 	}
-	filename = filename.match(/(\w*).js/g)[0];
 
-	this.info = info;
-	this.INFO = INFO;
-	this.debug = debug;
-	this.DEBUG = DEBUG;
-	this.trace = trace;
-	this.TRACE = TRACE;
-	this.table = table;
-	this.error = error;
-	this.level = levelAccessor;
-	return this;
-
-	function levelAccessor(arg) {
+	level(arg) {
 		if (arg) {
-			setLogLevel(arg);
+			_setLogLevel(arg);
 		} else {
 			return logLevel;
 		}
 	}
 
-	function setLogLevel(arg) {
-		let newLogLevel = String(arg).toLowerCase();
-		logLevel = newLogLevel;
-		Core = require(_PATH + 'src/core/Core.js').Core;
-		Core.conf('log', logLevel);
-		info();
-		INFO('--> Logger level set to:', logLevel);
-		if (newLogLevel == LEVEL.DEBUG || newLogLevel == LEVEL.TRACE) {
-			timeoutToInfoLevel(TIMEOUT);
-		}
+	info() {
+		console.log(Utils.logTime(timestamp), '[' + filename + ']', _formatLog(arguments));
 	}
 
-	var cancelTimeout;
-	function timeoutToInfoLevel(delay) {
-		info('back to info level in', delay, 'min');
-		clearTimeout(cancelTimeout);
-		cancelTimeout = setTimeout(() => {
-			levelAccessor() != LEVEL.INFO && levelAccessor(LEVEL.INFO);
-			Core.conf('log', LEVEL.INFO);
-		}, delay * 60 * 1000);
+	INFO() {
+		console.log(Utils.logTime(timestamp), '[' + filename.toUpperCase() + ']', _formatLog(arguments).toUpperCase());
 	}
 
-	function formatLog(args) {
-		if (typeof args === 'string') {
-			return args;
-		}
-		var log = '';
-		if (args[0] == '\n') {
-			console.log('');
-			delete args[0];
-		}
-		for (var i in args) {
-			if (typeof args[i] == 'object') {
-				log = log + util.format(util.inspect(args[i]) + ' ');
-			} else {
-				log = log + args[i] + ' ';
-			}
-		}
-		return log;
-	}
-
-	function info() {
-		console.log(Utils.logTime(timestamp), '[' + filename + ']', formatLog(arguments));
-	}
-
-	function INFO() {
-		console.log(Utils.logTime(timestamp), '[' + filename.toUpperCase() + ']', formatLog(arguments).toUpperCase());
-	}
-
-	function debug() {
+	debug() {
 		if (logLevel == LEVEL.DEBUG || logLevel == LEVEL.TRACE)
-			console.log(Utils.logTime(timestamp), '[' + filename + ']\u2022', formatLog(arguments));
+			console.log(Utils.logTime(timestamp), '[' + filename + ']\u2022', _formatLog(arguments));
 	}
 
-	function DEBUG() {
+	DEBUG() {
 		if (logLevel == LEVEL.DEBUG || logLevel == LEVEL.TRACE)
 			console.log(
 				Utils.logTime(timestamp),
 				'[' + filename.toUpperCase() + ']\u2022',
-				formatLog(arguments).toUpperCase()
+				_formatLog(arguments).toUpperCase()
 			);
 	}
 
-	function trace() {
+	trace() {
 		if (logLevel == LEVEL.TRACE)
-			console.log(Utils.logTime(timestamp), '[' + filename + ']\u2022\u2022', formatLog(arguments));
+			console.log(Utils.logTime(timestamp), '[' + filename + ']\u2022\u2022', _formatLog(arguments));
 	}
 
-	function TRACE() {
+	TRACE() {
 		if (logLevel == LEVEL.TRACE)
 			console.log(
 				Utils.logTime(timestamp),
 				'[' + filename.toUpperCase() + ']\u2022\u2022',
-				formatLog(arguments).toUpperCase()
+				_formatLog(arguments).toUpperCase()
 			);
 	}
 
-	function error() {
+	error() {
 		console.log('______________');
-		console.error(Utils.logTime(timestamp), '[' + filename + ']', /*'ERR >>',*/ formatLog(arguments));
+		console.error(Utils.logTime(timestamp), '[' + filename + ']', /*'ERR >>',*/ _formatLog(arguments));
 	}
 
-	/** Function to array an object */
-	function table(src, title, updatedEntries) {
-		let datas = formatObjectToTable(src, updatedEntries);
-		let tableSize = calculateTableSize(datas);
+	table(src, title, updatedEntries) {
+		let datas = _formatObjectToTable(src, updatedEntries);
+		let tableSize = _calculateTableSize(datas);
 		let logArrayTitle = '';
 		if (title) {
 			logArrayTitle = '│  ' + title + ' '.repeat(tableSize.col1 + tableSize.col2 + 2 - title.length) + ' │';
@@ -155,67 +104,128 @@ function Logger(filename, modeCore) {
 		});
 		console.log(confTable + '└' + '─'.repeat(tableSize.col1 + 2) + '┴' + '─'.repeat(tableSize.col2 + 2) + '┘');
 	}
+};
 
-	/** Return formated object */
-	function formatObjectToTable(obj, updatedEntries) {
-		let datas = {};
-		Object.keys(obj).forEach((key, index) => {
-			let updated = updatedEntries && Utils.searchStringInArray(key, updatedEntries) ? true : false;
-			let data = obj[key];
-			if (data == false || data == null || data == 0) return;
-			if (typeof data == 'object' && !Array.isArray(data)) {
-				Object.keys(data).forEach((key2, index2) => {
-					let data2 = data[key2];
-					if (data2) {
-						// not logging null entries
-						if (index2 == 0) {
-							datas[(updated ? '*' : '') + key] = [String(key2 + ': ' + getDataOrObject(data2))];
-						} else if (Array.isArray(datas[(updated ? '*' : '') + key])) {
-							datas[(updated ? '*' : '') + key].push(String(key2 + ': ' + getDataOrObject(data2)));
-						}
-					}
-				});
-			} else {
-				datas[(updated ? '*' : '') + key] = [String(data)];
-			}
-		});
-		return datas;
+// function Logger(filename, modeCore) {
+// 	Utils = require(_PATH + 'src/core/Utils.js');
+// 	if (modeCore && modeCore == 'sleep') {
+// 		timestamp = TIMESTAMP_PATTERN.SLEEP;
+// 	} else {
+// 		timestamp = TIMESTAMP_PATTERN.NORMAL;
+// 	}
+// 	filename = filename.match(/(\w*).js/g)[0];
+
+// 	this.info = info;
+// 	this.INFO = INFO;
+// 	this.debug = debug;
+// 	this.DEBUG = DEBUG;
+// 	this.trace = trace;
+// 	this.TRACE = TRACE;
+// 	this.table = table;
+// 	this.error = error;
+// 	this.level = levelAccessor;
+// 	return this;
+// }
+function _setLogLevel(arg) {
+	let newLogLevel = String(arg).toLowerCase();
+	logLevel = newLogLevel;
+	Core = require(_PATH + 'src/core/Core.js').Core;
+	Core.conf('log', logLevel);
+	info();
+	INFO('--> Logger level set to:', logLevel);
+	if (newLogLevel == LEVEL.DEBUG || newLogLevel == LEVEL.TRACE) {
+		_timeoutToInfoLevel(TIMEOUT);
 	}
+}
 
-	function getDataOrObject(data) {
-		if (typeof data == 'object' && !Array.isArray(data)) {
-			return util.inspect(data);
+var cancelTimeout;
+function _timeoutToInfoLevel(delay) {
+	info('back to info level in', delay, 'min');
+	clearTimeout(cancelTimeout);
+	cancelTimeout = setTimeout(() => {
+		level() != LEVEL.INFO && level(LEVEL.INFO);
+		Core.conf('log', LEVEL.INFO);
+	}, delay * 60 * 1000);
+}
+
+function _formatLog(args) {
+	if (typeof args === 'string') {
+		return args;
+	}
+	var log = '';
+	if (args[0] == '\n') {
+		console.log('');
+		delete args[0];
+	}
+	for (var i in args) {
+		if (typeof args[i] == 'object') {
+			log = log + util.format(util.inspect(args[i]) + ' ');
 		} else {
-			return data;
+			log = log + args[i] + ' ';
 		}
 	}
+	return log;
+}
 
-	/** Function to calculate array width */
-	function calculateTableSize(datas) {
-		let tableSize = { col1: [], col2: [] };
-		Object.keys(datas).forEach(key => {
-			tableSize.col1.push(key.length);
-			tableSize.col2.push(
-				Math.max.apply(
-					Math,
-					datas[key].map(el => {
-						return el.length;
-					})
-				)
-			);
-		});
-		tableSize.col1 = Math.max.apply(
-			Math,
-			tableSize.col1.map(el => {
-				return el;
-			})
-		);
-		tableSize.col2 = Math.max.apply(
-			Math,
-			tableSize.col2.map(el => {
-				return el;
-			})
-		);
-		return tableSize;
+/** Return formated object */
+function _formatObjectToTable(obj, updatedEntries) {
+	let datas = {};
+	Object.keys(obj).forEach((key, index) => {
+		let updated = updatedEntries && Utils.searchStringInArray(key, updatedEntries) ? true : false;
+		let data = obj[key];
+		if (data == false || data == null || data == 0) return;
+		if (typeof data == 'object' && !Array.isArray(data)) {
+			Object.keys(data).forEach((key2, index2) => {
+				let data2 = data[key2];
+				if (data2) {
+					// not logging null entries
+					if (index2 == 0) {
+						datas[(updated ? '*' : '') + key] = [String(key2 + ': ' + _getDataOrObject(data2))];
+					} else if (Array.isArray(datas[(updated ? '*' : '') + key])) {
+						datas[(updated ? '*' : '') + key].push(String(key2 + ': ' + _getDataOrObject(data2)));
+					}
+				}
+			});
+		} else {
+			datas[(updated ? '*' : '') + key] = [String(data)];
+		}
+	});
+	return datas;
+}
+
+function _getDataOrObject(data) {
+	if (typeof data == 'object' && !Array.isArray(data)) {
+		return util.inspect(data);
+	} else {
+		return data;
 	}
+}
+
+/** Function to calculate array width */
+function _calculateTableSize(datas) {
+	let tableSize = { col1: [], col2: [] };
+	Object.keys(datas).forEach(key => {
+		tableSize.col1.push(key.length);
+		tableSize.col2.push(
+			Math.max.apply(
+				Math,
+				datas[key].map(el => {
+					return el.length;
+				})
+			)
+		);
+	});
+	tableSize.col1 = Math.max.apply(
+		Math,
+		tableSize.col1.map(el => {
+			return el;
+		})
+	);
+	tableSize.col2 = Math.max.apply(
+		Math,
+		tableSize.col2.map(el => {
+			return el;
+		})
+	);
+	return tableSize;
 }
