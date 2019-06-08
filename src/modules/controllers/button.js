b628b8c91bd928c01f36109dc3b587c787872634
Adri <adrigarry@gmail.com>
2019-06-08 14:46:12 +0200

    6.6.14 Button refactor
    * 6.6.14 led refactor WIP
    
    * 6.6.14 button & buttonService refactor WIP
    
    * 6.6.14 buttonStats & etatButtonAction
    
    * 6.6.14 button Service stable
    
    * 6.6.14 buttonService renamed as buttonAction
    
    * 6.6.14 buttonAction fix
    
    * 6.6.14 button fix
    
    * 6.6.14 button refactor stabilized
    
    * 6.6.14 removing log.table(runtime)
    
    * 6.6.14 getEdgeButtonValue

--- src/modules/controllers/button.js
+++ src/modules/controllers/button.js
@@ -7,101 +7,64 @@ const Core = require(_PATH + 'src/core/Core.js').Core,
 	log = new (require(Core._CORE + 'Logger.js'))(__filename),
 	Utils = require(Core._CORE + 'Utils.js');
 
-var belly = new Gpio(17, 'out'); // TODO...
-const DEBOUNCE_LIMIT = 0.4;
-var Button = {};
+var Button = {},
+	LED_FLAG;
+
+Core.gpio.leds.forEach(led => {
+	if (led.id == 'buttonFlag') LED_FLAG = new Gpio(led.pin, led.direction);
+});
+if (!LED_FLAG) Core.error('LED_FLAG not initialized!');
 
 Core.gpio.buttons.forEach(button => {
 	Button[button.id] = new Gpio(button.pin, button.direction, button.edge, button.options);
-	Button[button.id]['id'] = Utils.capitalizeFirstLetter(button.id);
+	Button[button.id]['id'] = button.id;
+	Button[button.id]['name'] = Utils.capitalizeFirstLetter(button.id);
+	Button[button.id]['edge'] = button.edge;
+	Core.run('buttonStats.' + button.id, 0);
 });
 
-setImmediate(() => {
-	Core.do('interface|sound|volume', Core.isAwake() ? (Button.etat.readSync() ? 100 : 50) : 0);
+Object.keys(Button).forEach(id => {
+	watchButton(Button[id]);
+	getEdgeButtonValue(Button[id]);
 });
 
-var instance = false,
-	intervalEtat;
-const INTERVAL_DELAY = (Core.conf('watcher') ? 60 : 5 * 60) * 1000; //3 * 60 * 1000;
-setInterval(function() {
-	// TODO A deplacer dans flux.next('service|context|refresh')) ?
-	let value = Button.etat.readSync();
-	// TODO faire un truc avec ce flux => move to jobsList.json?
-	Core.do('interface|led|toggle', { leds: ['satellite'], value: value }, { log: 'trace' });
-	Core.run('etat', value ? 'high' : 'low');
-
-	if (1 === value) {
-		if (!instance) {
-			// TODO! deplacer ça dans le handler ... !?
-			instance = true;
-			intervalEtat = setInterval(function() {
-				log.info('Etat btn Up => random action');
-				Core.do('service|interaction|random');
-			}, INTERVAL_DELAY);
-			Core.do('service|video|loop');
-		}
-	} else {
-		instance = false;
-		clearInterval(intervalEtat);
-	}
-}, 2000);
-function getPushTime(button) {
-	belly.writeSync(1);
-	let pushedTime = new Date();
-	while (button.readSync() == 1) {
-		var time = Math.round((new Date() - pushedTime) / 100) / 10;
-		if (time % 1 == 0) {
-			belly.writeSync(0);
-		} else {
-			belly.writeSync(1);
-		}
-	}
-	//log.info('Button must be pushed for ' + DEBOUNCE_LIMIT + 's at least, try again!');
-	belly.writeSync(0);
-	let pushTime = Math.round((new Date() - pushedTime) / 100) / 10;
-	log.info(button.id + ' button pressed for ' + pushTime + ' sec...');
-	return pushTime;
+function watchButton(button) {
+	button.watch((err, value) => {
+		if (err) Core.error('Button error', err);
+		let buttonData = getButtonData(button);
+		Core.do('controller|button|' + button.id, buttonData);
+	});
 }
 
-Button.ok.watch((err, value) => {
-	var pushTime = getPushTime(Button.ok);
-	Core.do('controller|button|ok', pushTime);
-});
-
-Button.cancel.watch((err, value) => {
-	Core.do('interface|sound|mute');
-	var pushTime = getPushTime(Button.cancel);
-	Core.do('controller|button|cancel', pushTime);
-});
-
-Button.white.watch((err, value) => {
-	var pushTime = getPushTime(Button.white);
-	Core.do('controller|button|white', pushTime);
-});
-
-Button.blue.watch((err, value) => {
-	var pushTime = getPushTime(Button.blue);
-	if (pushTime > DEBOUNCE_LIMIT) Core.do('controller|button|blue', pushTime);
-	else {
-		// already done in the handler
-		log.info('Blue button pushed not enough:', pushTime);
-		log.info('___This should not be logged any more !!!');
-		// TODO refactor to get this unified with buttonService same code
-		Core.run('buttonStats.blueError', Core.run('buttonStats.blueError') + 1);
+function getEdgeButtonValue(button) {
+	if (button.edge == 'both') {
+		setTimeout(() => {
+			let buttonData = getButtonData(button);
+			Core.do('controller|button|' + button.id, buttonData);
+		}, 100);
 	}
-});
+}
 
-/** Switch watch for radio volume */
-Button.etat.watch((err, value) => {
-	value = Button.etat.readSync();
-	Core.run('etat', value ? 'high' : 'low');
-	log.info('Etat has changed:', Core.run('etat'));
-	let newVolume = Core.isAwake() ? (value ? 100 : 50) : 0;
-	Core.do('interface|sound|volume', newVolume);
-	if (Core.run('screen')) {
-		Core.do('interface|hdmi|off');
+function getButtonData(button) {
+	if (button.edge == 'rising') {
+		return getPushTime(button);
+	} else if (button.edge == 'both') {
+		return button.readSync();
+	} else {
+		return;
 	}
-	setTimeout(() => {
-		log.table(Core.run(), 'RUNTIME');
-	}, 200);
-});
+}
+
+function getPushTime(button) {
+	LED_FLAG.writeSync(1);
+	let startPushTime = new Date();
+	while (button.readSync() == 1) {
+		var time = Math.round((new Date() - startPushTime) / 100) / 10;
+		if (time % 1 == 0) LED_FLAG.writeSync(0);
+		else LED_FLAG.writeSync(1);
+	}
+	LED_FLAG.writeSync(0);
+	let pushTime = Math.round((new Date() - startPushTime) / 100) / 10;
+	log.info(button.name + ' button pressed for ' + pushTime + ' sec...');
+	return pushTime;
+}
---
+++ src/modules/services/buttonAction.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+'use strict';
+
+const Core = require(_PATH + 'src/core/Core.js').Core,
+	log = new (require(Core._CORE + 'Logger.js'))(__filename.match(/(\w*).js/g)[0]),
+	Utils = require(Core._CORE + 'Utils.js');
+
+// const BTN_PUSH_MIN = 0.4;
+
+Core.flux.controller.button.subscribe({
+	next: flux => {
+		buttonHandler(flux);
+	},
+	error: err => {
+		Core.error('Flux error', err);
+	}
+});
+
+function buttonHandler(flux) {
+	if (flux.id == 'ok') {
+		okButtonAction(flux.value);
+	} else if (flux.id == 'cancel') {
+		cancelButtonAction(flux.value);
+	} else if (flux.id == 'white') {
+		whiteButtonAction(flux.value);
+	} else if (flux.id == 'blue') {
+		blueButtonAction(flux.value);
+	} else if (flux.id == 'etat') {
+		etatButtonAction(flux.value);
+	} else {
+		log.error('Unkown button', flux);
+		return;
+	}
+	Core.run('buttonStats.' + flux.id, Core.run('buttonStats.' + flux.id) + 1);
+}
+
+function okButtonAction(duration) {
+	if (Core.isAwake()) {
+		/*if (Core.run('timer')) {
+			Core.do('service|music|playlist', Core.run('music'));
+		} else */
+		if (Core.run('voicemail')) {
+			Core.do('service|voicemail|check');
+		} else if (Core.run('audioRecord')) {
+			Core.do('service|audioRecord|check');
+		} else if (Core.run('music')) {
+			Core.do('service|music|jukebox', Core.run('music'));
+			// Core.do('service|music|playlist', Core.run('music'));
+		} else if (Core.run('mood').indexOf('party') > -1) {
+			if (Utils.rdm()) {
+				Core.do('service|party|tts');
+			} else {
+				Core.do('service|mood|badBoy');
+			}
+		} else {
+			Core.do('service|interaction|random');
+		}
+	} else {
+		Core.do('service|context|restart');
+	}
+}
+function cancelButtonAction(duration) {
+	Core.do('interface|sound|mute');
+	if (duration < 1) {
+		// Mute, do nothing
+	} else if (duration >= 1 && duration < 3) {
+		Core.do('service|context|restart');
+	} else if (duration >= 3 && duration < 6) {
+		Core.do('service|context|restart', 'sleep');
+	} else if (duration > 6) {
+		Core.do('service|context|restart', 'test');
+	} else Core.error('Button->else', flux);
+}
+
+function whiteButtonAction(duration) {
+	if (Core.isAwake()) {
+		Core.do('service|timer|increase', Math.round(duration));
+	} else {
+		Core.do('service|system|light', duration * 60);
+	}
+}
+
+function blueButtonAction(duration) {
+	if (Core.run('etat')) {
+		Core.do('service|music|fip');
+	} else {
+		Core.do('service|music|jukebox');
+	}
+}
+
+function etatButtonAction(value) {
+	Core.run('etat', value ? 'high' : 'low');
+	log.info('Etat button value:', Core.run('etat'));
+	Core.do('interface|led|toggle', { leds: ['satellite'], value: value }, { log: 'trace' });
+	let newVolume = Core.isAwake() ? (value ? 100 : 50) : 0;
+	Core.do('interface|sound|volume', newVolume, { log: 'debug' });
+	if (Core.run('screen')) {
+		Core.do('interface|hdmi|off');
+	}
+	etatInteraction(value);
+}
+
+var instance = false,
+	intervalEtat;
+const INTERVAL_DELAY = (Core.conf('watcher') ? 60 : 5 * 60) * 1000; //3 * 60 * 1000;
+function etatInteraction(value) {
+	if (1 === value) {
+		if (!instance) {
+			instance = true;
+			intervalEtat = setInterval(function() {
+				log.info('Etat btn Up => random action');
+				Core.do('service|interaction|random');
+			}, INTERVAL_DELAY);
+			Core.do('service|video|loop');
+		}
+	} else {
+		instance = false;
+		clearInterval(intervalEtat);
+	}
+}
