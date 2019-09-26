cdb7fbefde71f1221913cfa780a2fd060559094b
adri <adrigarry@gmail.com>
2019-09-26 22:09:09 +0200

    6.8.3 cerbot update

--- src/shell/certbot.sh
+++ src/shell/certbot.sh
@@ -2,12 +2,11 @@
 
 #https://certbot.eff.org/lets-encrypt/pip-other
 
-sudo wget -P /home/odi/core/tmp/ https://dl.eff.org/certbot-auto
+#sudo wget -P /home/odi/core/tmp/ https://dl.eff.org/certbot-auto
+#sudo chmod a+x /home/odi/core/tmp/certbot-auto
+#sudo /home/odi/core/tmp/certbot-auto certonly --webroot -w /home/odi/core/src/web -d odi.adrigarry.com
 
-sudo chmod a+x /home/odi/core/tmp/certbot-auto
-
-sudo /home/odi/core/tmp/certbot-auto certonly --webroot -w /home/odi/core/src/web -d odi.adrigarry.com
+sudo ./certbot-auto renew
 
 sudo cp /etc/letsencrypt/live/odi.adrigarry.com/fullchain.pem /home/odi/core/security/cert.pem
-
 sudo cp /etc/letsencrypt/live/odi.adrigarry.com/privkey.pem /home/odi/core/security/key.pem
