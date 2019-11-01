a9b6c373774485512f3e246766ab3c72e3cc5708
adri <adrigarry@gmail.com>
2019-11-01 04:16:22 +0100

    adding radiator.js service

---
+++ src/modules/services/radiator.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const Core = require(_PATH + 'src/core/Core.js').Core,
+	log = new (require(Core._CORE + 'Logger.js'))(__filename),
+	Utils = require(Core._CORE + 'Utils.js');
+
+module.exports = {};
+
+Core.flux.service.radiator.subscribe({
+	next: flux => {
+		if (flux.id == '') {
+			//
+		} else Core.error('unmapped flux in Radiator service', flux, false);
+	},
+	error: err => {
+		Core.error('Flux error', err);
+	}
+});
