be525c51873e22030dc4aa99d800146900537bb0
adri <adrigarry@gmail.com>
2019-11-23 03:26:54 +0100

    scheduleSend

--- src/modules/services/radiator.js
+++ src/modules/services/radiator.js
@@ -6,12 +6,18 @@ const Core = require(_PATH + 'src/core/Core.js').Core,
 	log = new (require(Core._CORE + 'Logger.js'))(__filename),
 	Utils = require(Core._CORE + 'Utils.js');
 
-module.exports = {};
+module.exports = {
+	cron: {
+		base: [{ cron: '10 */10 * * * *', flux: { id: 'service|radiator|scheduleSend' } }]
+	}
+};
 
 Core.flux.service.radiator.subscribe({
 	next: flux => {
 		if (flux.id == 'setState') {
 			setState(flux.value);
+		} else if (flux.id == 'scheduleSend') {
+			scheduleSend();
 		} else if (flux.id == 'timer') {
 			timer(flux.value);
 		} else if (flux.id == 'scheduler') {
@@ -30,6 +36,11 @@ setImmediate(() => {
 // on/off
 function setState(state) {
 	log.info('Radiator.state:', state);
+}
+
+// scheduleSend
+function scheduleSend() {
+	log.info('Radiator.scheduleSend...');
 }
 
 // timer
