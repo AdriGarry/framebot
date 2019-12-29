c13eb276f7271ec8bbc046808ff8c1fab56cb872
adrigarry <adrigarry@gmail.com>
2019-12-29 20:01:56 +0100

    new rfxcom interface

---
+++ src/modules/interfaces/rfxcom.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+'use strict';
+
+const Core = require(_PATH + 'src/core/Core.js').Core,
+	log = new (require(Core._CORE + 'Logger.js'))(__filename);
+
+const rfxcom = require('rfxcom'),
+	rfxtrx = new rfxcom.RfxCom('/dev/ttyUSB0', { debug: true });
+
+module.exports = {};
+
+Core.flux.interface.rfxcom.subscribe({
+	next: flux => {
+		if (flux.id == 'status') {
+			setDeviceStatus(flux.value);
+		} else {
+			Core.error('unmapped flux in Rfxcom interface', flux, false);
+		}
+	},
+	error: err => {
+		Core.error('Flux error', err);
+	}
+});
+
+setImmediate(() => {
+	// do something, or useless?
+});
+
+setTimeout(() => {
+	Core.do('interface|rfxcom|status', { device: 'plugB', value: true });
+	Core.do('interface|rfxcom|status', { device: 'plugB', value: false }, { delay: 10 });
+}, 13000);
+
+let ready = false;
+
+rfxtrx.initialise(function() {
+	log.info('Rfxcom gateway ready\n');
+	ready = true;
+});
+
+const DEVICE = new rfxcom.Lighting2(rfxtrx, rfxcom.lighting2.AC);
+
+///////////////////////////
+
+rfxtrx.on('receive', function(evt) {
+	log.debug('Rfxcom_receive:', Buffer.from(evt).toString('hex'));
+});
+
+const arrayToObject = array =>
+	array.reduce((obj, item) => {
+		obj[item.name] = item;
+		return obj;
+	}, {});
+
+const DEVICE_LIST = arrayToObject(Core.descriptor.rfxcom);
+console.log(DEVICE_LIST);
+
+function setDeviceStatus(args) {
+	log.info('setDeviceStatus', args);
+	if (!ready) {
+		log.warn('rfxcom not initialized!');
+		return;
+	}
+	let deviceName = args.device,
+		value = args.value;
+	// let knownDevice = DEVICE_LIST.filter(device => device.name === deviceName).length > 0;
+	let knownDevice = DEVICE_LIST.hasOwnProperty(deviceName);
+	log.info(DEVICE_LIST[deviceName]);
+	if (knownDevice) {
+		if (value) DEVICE.switchOn(DEVICE_LIST[deviceName].id);
+		else DEVICE.switchOff(DEVICE_LIST[deviceName].id);
+	} else log.info('Unreconized device:', deviceName);
+}
