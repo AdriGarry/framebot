3b816b81f9702ecabcbe7e57f72acee4f77cfc80
adrigarry <adrigarry@gmail.com>
2020-02-25 19:38:33 +0100

    Adding ttsClass

---
+++ src/core/ttsClass.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict';
+
+const Core = require(_PATH + 'src/core/Core.js').Core,
+	log = new (require(Core._CORE + 'Logger.js'))(__filename);
+
+const FALLBACK_LANGUAGE = 'fr',
+	FALLBACK_VOICE = 'espeak';
+
+class TTS {
+	constructor(message, language, voice) {
+		this.msg = message;
+		if (language) {
+			this.lg = language;
+		} else {
+			log.debug('No valid language, fallback on Fr');
+			this.lg = FALLBACK_LANGUAGE;
+		}
+		if (voice) {
+			this.voice = voice;
+		} else {
+			log.debug('No valid voice, fallback on espeak');
+			this.voice = FALLBACK_VOICE;
+		}
+	}
+
+	getMsg() {
+		return this.msg;
+	}
+
+	getLg() {
+		return this.lg;
+	}
+
+	getVoice() {
+		return this.voice;
+	}
+
+	setMsgReplaced() {
+		this.msg = this.msg.replace('%20', '');
+		return this.msg;
+	}
+}
+module.exports = TTS;
