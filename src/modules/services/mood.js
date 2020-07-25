7d52f3780e6ffed4a0b2cf5d81d9e9322e369bb7
adrigarry <adrigarry@gmail.com>
2020-07-25 18:06:12 +0200

    mood levels basic function

--- src/modules/services/mood.js
+++ src/modules/services/mood.js
@@ -12,7 +12,22 @@ const log = new Logger(__filename);
 
 
 const FLUX_PARSE_OPTIONS = [
+	{ set: setMoodLevel }
 ];
 
 Observers.attachFluxParseOptions('service', 'mood', FLUX_PARSE_OPTIONS);
 
+const MOOD_LEVELS = {
+	0: { volume: 0 },  // muted
+	1: { volume: 30 }, // system tts (including timer, and others human triggered functions)
+	2: { volume: 50 }, // information tts (weather, hour, today...)
+	3: { volume: 60 }, // interactive tts + exclamation sounds + max
+	4: { volume: 80 }, // screen/diapo
+	5: { volume: 100 } // party mode + pirate ?
+};
+
+function setMoodLevel(moodLevelId) {
+	log.info('Mood level set to', moodLevelId);
+	let newMoodLevel = MOOD_LEVELS[moodLevelId];
+	new Flux('interface|sound|volume', newMoodLevel.volume);
+}
