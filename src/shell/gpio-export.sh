45d71f47fdd60f0c541e14c5611889351e2a02f2
adri <adrigarry@gmail.com>
2020-09-22 20:51:18 +0200

    7.6.8 Add gpio-export script

---
+++ src/shell/gpio-export.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo gpio export 14 out
+sudo gpio export 15 out
+sudo gpio export 23 out
+sudo gpio export 4 out
+sudo gpio export 20 in
+sudo gpio export 16 in
+sudo gpio export 19 in
+sudo gpio export 26 in
+sudo gpio export 13 in
