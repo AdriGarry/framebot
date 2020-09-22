6e41b8362523c23f9c086b39377fc0bdb4fdb4f6
adri <adrigarry@gmail.com>
2020-09-22 20:58:25 +0200

    7.6.8 Update certbot script

--- src/shell/certbot.sh
+++ src/shell/certbot.sh
@@ -2,11 +2,11 @@
 
 #https://certbot.eff.org/lets-encrypt/pip-other
 
-sudo wget -P /home/odi/framebot/tmp/ https://dl.eff.org/certbot-auto
-sudo chmod a+x /home/odi/framebot/tmp/certbot-auto
-sudo /home/odi/framebot/tmp/certbot-auto certonly --webroot -w /home/odi/framebot/src/web -d odi.adrigarry.com
+wget -P /home/odi/framebot/tmp/ https://dl.eff.org/certbot-auto
+chmod a+x /home/odi/framebot/tmp/certbot-auto
+/home/odi/framebot/tmp/certbot-auto certonly --webroot -w /home/odi/framebot/src/web -d odi.adrigarry.com
 
-# sudo ./certbot-auto renew
+# ./certbot-auto renew
 
-sudo cp /etc/letsencrypt/live/odi.adrigarry.com/fullchain.pem /home/odi/framebot/security/cert.pem
-sudo cp /etc/letsencrypt/live/odi.adrigarry.com/privkey.pem /home/odi/framebot/security/key.pem
+cp /etc/letsencrypt/live/odi.adrigarry.com/fullchain.pem /home/odi/framebot/security/cert.pem
+cp /etc/letsencrypt/live/odi.adrigarry.com/privkey.pem /home/odi/framebot/security/key.pem
