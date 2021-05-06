0e913cf3baffef6588e8b227505d897338834156
AdriGarry <adrigarry@gmail.com>
2021-05-06 02:34:41 +0200

    Update TestModuleClass

--- src/modules/services/TestModuleClass.js
+++ src/modules/services/TestModuleClass.js
@@ -2,34 +2,42 @@ const Module = require('../../api/Module');
 const Logger = require('../../api/Logger');
 const log = new Logger(__filename);
 
-module.exports = class TestModuleClass extends Module {
-   constructor(arg) {
-      super();
-      this.crons = {
-         full: [
-            { cron: '5 0 0 * * 1-5', flux: { id: 'service|context|goToSleep' } },
-            { cron: '5 0 2 * * 0,6', flux: { id: 'service|context|goToSleep' } },
-            {
-               cron: '13 13 13 * * 1-6',
-               flux: [
-                  { id: 'interface|tts|speak', data: { lg: 'en', msg: 'Auto restart' } },
-                  { id: 'service|context|restart', conf: { delay: 3 } }
-               ]
-            },
-            {
-               cron: '13 13 13 * * 0',
-               flux: [
-                  { id: 'interface|tts|speak', data: { lg: 'en', msg: 'Reset config' } },
-                  { id: 'service|context|reset', conf: { delay: 3 } }
-               ]
-            }
+let crons = {
+   full: [
+      { cron: '5 0 0 * * *', flux: { id: 'service|context|goToSleep' } },
+      {
+         cron: '13 13 13 * * 1-6',
+         flux: [
+            { id: 'interface|tts|speak', data: { lg: 'en', msg: 'Auto restart' } },
+            { id: 'service|context|restart', conf: { delay: 3 } }
          ]
-      };
+      },
+      {
+         cron: '13 13 13 * * 0',
+         flux: [
+            { id: 'interface|tts|speak', data: { lg: 'en', msg: 'Reset config' } },
+            { id: 'service|context|reset', conf: { delay: 3 } }
+         ]
+      }
+   ]
+};
+
+module.exports = class TestModuleClass extends Module {
+   constructor(arg) { // constructor args ?
+      log.test('TestModuleClass constructor')
+      super(crons);
+      // => flux parser etc here...
+      //Observers.attachFluxParser('service', 'context', contextHandler);
    }
 
    onInit() {
+      // Set module as ready now, or after stuff (like rfxcom gateway ready)?
       log.test('new test module as class instanciation');
       privateMethod();
+   }
+
+   onDestroy() {
+      log.test('onDestroy...')
    }
 }
 
@@ -37,25 +45,23 @@ function privateMethod() {
    log.test('inside private method!')
 }
 
-// const exportData = {
-//    cron: {
-//       full: [
-//          { cron: '5 0 0 * * 1-5', flux: { id: 'service|context|goToSleep' } },
-//          { cron: '5 0 2 * * 0,6', flux: { id: 'service|context|goToSleep' } },
-//          {
-//             cron: '13 13 13 * * 1-6',
-//             flux: [
-//                { id: 'interface|tts|speak', data: { lg: 'en', msg: 'Auto restart' } },
-//                { id: 'service|context|restart', conf: { delay: 3 } }
-//             ]
-//          },
-//          {
-//             cron: '13 13 13 * * 0',
-//             flux: [
-//                { id: 'interface|tts|speak', data: { lg: 'en', msg: 'Reset config' } },
-//                { id: 'service|context|reset', conf: { delay: 3 } }
-//             ]
-//          }
-//       ]
-//    }
-// };
+
+// Observers.attachFluxParser('service', 'context', contextHandler);
+
+function contextHandler(flux) {
+   if (flux.id == 'restart') {
+      restartCore(flux.value);
+   } else if (flux.id == 'sleep') {
+      restartCore('sleep');
+   } else if (flux.id == 'sleepForever') {
+      updateConf({ mode: 'sleep', alarms: { weekDay: null, weekEnd: null } }, true);
+   } else if (flux.id == 'goToSleep') {
+      goToSleep();
+   } else if (flux.id == 'update') {
+      updateConf(flux.value, false);
+   } else if (flux.id == 'updateRestart') {
+      updateConf(flux.value, true);
+   } else if (flux.id == 'reset') {
+      resetCore();
+   } else Core.error('unmapped flux in Context service', flux, false);
+}
