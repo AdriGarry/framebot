92427b15e16e86c579b1da5e2223219d564f7ff2
AdriGarry <adrigarry@gmail.com>
2021-10-25 03:40:12 +0200

    Add Utils.formatStringLength tests

--- src/test/unit/Utils.test.js
+++ src/test/unit/Utils.test.js
@@ -5,13 +5,48 @@ const assert = require('assert');
 
 const Utils = require('./../../api/Utils');
 
-describe('Utils', function () {
-   describe('Utils.repeatString', function () {
-      it('should return given string concatenated X times', function () {
+console.log(Object.getOwnPropertyNames(Utils))
+
+describe('Utils', () => {
+   describe('Utils.repeatString', () => {
+      it('should return given string concatenated X times', () => {
          const given = 'abc',
             expected = 'abcabcabc';
          const result = Utils.repeatString(given, 3);
          assert.equal(expected, result);
+      });
+   });
+
+   describe('Utils.formatStringLength', () => {
+      it('should add space at the end of string to math expected length', () => {
+         const given = 'abcde',
+            expected = 'abcde  ';
+         const result = Utils.formatStringLength(given, 7);
+         assert.strictEqual(result, expected);
+      });
+
+      it('should add space at the begening of string to math expected length', () => {
+         const given = 'abcde',
+            expected = '  abcde';
+         const result = Utils.formatStringLength(given, 7, true);
+         assert.strictEqual(result, expected);
+      });
+
+      it('should truncate string to math expected length', () => {
+         const given = 'abcdefg',
+            expected = 'abcde';
+         const result = Utils.formatStringLength(given, 5);
+         assert.strictEqual(result, expected);
+      });
+   });
+
+   describe('Utils.toto', () => {
+      xit('toto... TODO', () => {
+         // const given = 'abcdefghijklmno',
+         //    expected = 'abcdefg';
+         // const result = Utils.formatStringLength(given, 7);
+         // assert.strictEqual(expected, result);
+         // console.log(expected, result);
       });
    });
 });
--- src/test/unit/index.test.js
+++ src/test/unit/index.test.js
@@ -1,30 +1,22 @@
 #!/usr/bin/env node
 'use strict';
 
-global._PATH = "./../../"; // Hack to assignate global _PATH constant
-
 const fs = require('fs'),
-   path = require('path'),
-   assert = require('assert');
+   path = require('path');
 
+global._PATH = "./../../"; // Hack to assignate global _PATH constant
 const UNIT_TEST_INDEX_FILENAME = __filename.split(/[\\/]/).pop();
 
-console.log('Mocha test sequence...', new Date())
-
-describe('Array', function () {
-   describe('#indexOf()', function () {
-      it('should return -1 when the value is not present', function () {
-         assert.equal([1, 2, 3].indexOf(4), -1);
-      });
+describe('Unit test sequence with Mocha', () => {
+   it('Loading all tests...', () => {
+      loadUnitTestFiles();
    });
 });
 
-loadUnitTestFiles();
 
 function loadUnitTestFiles() {
    fs.readdir(__dirname, (err, files) => {
       if (err) console.error(err);
-      console.log('Unit test files', files);
       files.forEach(testFile => {
          if (UNIT_TEST_INDEX_FILENAME !== testFile) {
             require(__dirname + path.sep + testFile);
