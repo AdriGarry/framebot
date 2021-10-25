c54e39b4f485647725097666c96db3c0912bc2f0
Adri <adrigarry@gmail.com>
2021-10-25 18:45:41 +0200

    7.9.0 Unit tests (#99)
    * Add mocha & first test
    
    * Set npm test script
    
    * Load unit test files from index.test
    
    * Add Utils.formatStringLength tests
    
    * Add yml script to run test
    
    * Rename auto tag yml script
    
    * Bump minor version

---
+++ src/test/unit/Utils.test.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+'use strict';
+
+const assert = require('assert');
+
+const Utils = require('./../../api/Utils');
+
+console.log(Object.getOwnPropertyNames(Utils))
+
+describe('Utils', () => {
+   describe('Utils.repeatString', () => {
+      it('should return given string concatenated x times', () => {
+         const given = 'abc',
+            expected = 'abcabcabc';
+         const result = Utils.repeatString(given, 3);
+         assert.strictEqual(expected, result);
+      });
+   });
+
+   describe('Utils.formatStringLength', () => {
+      it('should add space at the end of string to math expected length', () => {
+         const given = 'abcde',
+            expected = 'abcde  ';
+         const result = Utils.formatStringLength(given, 7);
+         assert.strictEqual(result, expected);
+      });
+
+      it('should add space at the begening of string to math expected length', () => {
+         const given = 'abcde',
+            expected = '  abcde';
+         const result = Utils.formatStringLength(given, 7, true);
+         assert.strictEqual(result, expected);
+      });
+
+      it('should truncate string to math expected length', () => {
+         const given = 'abcdefg',
+            expected = 'abcde';
+         const result = Utils.formatStringLength(given, 5);
+         assert.strictEqual(result, expected);
+      });
+   });
+
+   describe('Utils.toto', () => {
+      xit('toto... TODO', () => {
+         // const given = 'abcdefghijklmno',
+         //    expected = 'abcdefg';
+         // const result = Utils.formatStringLength(given, 7);
+         // assert.strictEqual(expected, result);
+         // console.log(expected, result);
+      });
+   });
+});
+
+
+// codePosition
+// repeatString
+// formatStringLength
+// deleteFolderRecursive
+// appendJsonFile
+// directoryContent
+// getJsonFileContent
+// searchStringInArray
+// arrayToObject
+// testConnection
+// getLocalIp
+// getPublicIp
+// postOdi
+// execCmd
+// debounce
+// throttle
+// getAbsolutePath
+// getDuration
+// firstLetterUpper
+// executionTime
+// addPatternBefore
+// formatDuration
+// numberWithDot
+// perCent
+// rdm
+// random
+// randomItem
+// delay
+// delayMs
+// logTime
+// capitalizeFirstLetter
+// getNextDateObject
+// isWeekend
---
+++ src/test/unit/index.test.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs'),
+   path = require('path'),
+   assert = require('assert');
+
+global._PATH = "./../../"; // Hack to set global _PATH constant
+const UNIT_TEST_INDEX_FILENAME = __filename.split(/[\\/]/).pop();
+
+describe('Unit test sequence with Mocha', () => {
+   it('Loading all tests...', () => {
+      loadUnitTestFiles();
+   });
+});
+
+
+function loadUnitTestFiles() {
+   fs.readdir(__dirname, (err, files) => {
+      if (err) console.error(err);
+      files.forEach(testFile => {
+         if (UNIT_TEST_INDEX_FILENAME !== testFile) {
+            require(__dirname + path.sep + testFile);
+            assert.ok(true);
+         }
+      })
+   })
+}
