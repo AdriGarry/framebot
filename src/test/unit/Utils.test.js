f333f24905f56ca289679dde0f8bcedbe7adbe41
AdriGarry <adrigarry@gmail.com>
2021-10-30 19:41:36 +0200

    Add delay & delayMs unit tests

--- src/test/unit/Utils.test.js
+++ src/test/unit/Utils.test.js
@@ -144,39 +144,36 @@ describe('Utils', function () {
    });
 
    describe('Utils.delay', function () {
-      xit('TODO...', function () {
-         // const given = 'abcdefghijklmno',
-         //    expected = 'abcdefg';
-         // const result = Utils.formatStringLength(given, 7);
-         // assert.strictEqual(expected, result);
-      });
-   });
-
-   describe('Utils.getLocalIp', function () {
-      xit('TODO...', function () {
-         // const given = 'abcdefghijklmno',
-         //    expected = 'abcdefg';
-         // const result = Utils.formatStringLength(given, 7);
-         // assert.strictEqual(expected, result);
-      });
-   });
-
-   describe('Utils.getPublicIp', function () {
-      xit('TODO...', function () {
-         // const given = 'abcdefghijklmno',
-         //    expected = 'abcdefg';
-         // const result = Utils.formatStringLength(given, 7);
-         // assert.strictEqual(expected, result);
+      it('should wait 0.3s before trigger callback', function (done) {
+         let given = true;
+         let result = false;
+         Utils.delay(0.3).then(function () {
+            result = given;
+         });
+         setTimeout(() => {
+            assert.ok(!result)
+         }, 200);
+         setTimeout(() => {
+            assert.ok(result)
+            done();
+         }, 300);
       });
    });
 
    describe('Utils.delayMs', function () {
-      it('TODO...', function (done) {
-
-         // const given = 'abcdefghijklmno',
-         //    expected = 'abcdefg';
-         // const result = Utils.formatStringLength(given, 7);
-         // assert.strictEqual(expected, result);
+      it('should wait 50ms before trigger callback', function (done) {
+         let given = true;
+         let result = false;
+         Utils.delayMs(50).then(function () {
+            result = given;
+         });
+         setTimeout(() => {
+            assert.ok(!result)
+         }, 10);
+         setTimeout(() => {
+            assert.ok(result)
+            done();
+         }, 50);
       });
    });
 
