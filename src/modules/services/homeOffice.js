daa1527ffea0bc88a538b67063d3c61e134ac538
AdriGarry <adrigarry@gmail.com>
2022-01-31 21:55:05 +0100

    Finalize homeOffice first version

--- src/modules/services/homeOffice.js
+++ src/modules/services/homeOffice.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
+const CronJob = require('cron').CronJob;
+
 const { Core, Flux, Logger, Observers } = require('../../api');
 
 const log = new Logger(__filename);
@@ -15,15 +17,21 @@ const FLUX_PARSE_OPTIONS = [{ id: 'start', fn: startHomeOffice }];
 
 Observers.attachFluxParseOptions('service', 'homeOffice', FLUX_PARSE_OPTIONS);
 
+const HOME_OFFICE_MOOD_LEVEL = 3;
+
 function startHomeOffice() {
-  log.info('startHomeoffice');
-  // TODO Test isAwake ...
+  log.info('Starting Homeoffice');
   setupHomeOffice();
-  Scheduler.delay(6 * 60 * 60).then(resetHomOffice);
+  new CronJob('* 16 17 * * *', function () {
+    stopHomeOffice();
+  });
 }
 
-function resetHomOffice() {
-  log.info('Reset home office functions... TODO !');
+function stopHomeOffice() {
+  log.info('Stopping home office');
+  new Flux('interface|tts|speak', { lg: 'en', msg: 'Stopping home office and restarting...' });
+  new Flux('service|powerPlug|toggle', { plug: 'plugA', mode: false });
+  new Flux('service|context|restart', null, { delay: 5 });
 }
 
 function setupHomeOffice() {
@@ -32,13 +40,16 @@ function setupHomeOffice() {
   // Desktop plug ON ~after 10 sec
   new Flux('service|powerPlug|toggle', { plug: 'plugA', mode: true }, { delay: 10 });
 
-  // Mood
-  new Flux('service|mood|set', 4);
+  // Radiator ON for 6 hours
+  new Flux('service|radiator|timeout', { mode: 'on', timeout: 6 * 60 });
 
-  // Mood to 0 at 12:30 + TTS ?
-  // new Flux('service|mood|set', 4);
-  // Mood to 0 at 1:45 + TTS ?
-  // new Flux('service|mood|set', 4);
+  setQuietModeDuringLunchTime();
+  setInteractions();
+}
+
+function setInteractions() {
+  // Mood
+  new Flux('service|mood|set', HOME_OFFICE_MOOD_LEVEL);
 
   // Clockwork ~each 13min
   new Flux('service|time|now', null, { delay: 13 * 60, loop: 30 });
@@ -46,6 +57,19 @@ function setupHomeOffice() {
   // Russia ~each 50min
   new Flux('service|interaction|russia', null, { delay: 50 * 60, loop: 10 });
 
-  // Desktop plug OFF at 5:30pm
-  // new Flux('service|powerPlug|toggle', { plug: 'plugA', mode: false });
+  // Go pickup Louloutes
+  new CronJob('* 15 17 * * *', function () {
+    new Flux('service|max|playHornSiren');
+    new Flux('interface|tts|speak', 'Go chercher les Louloutes!', { delay: 5 });
+  });
+}
+
+function setQuietModeDuringLunchTime() {
+  new CronJob('* 15 12 * * *', function () {
+    new Flux('interface|tts|speak', { lg: 'en', msg: 'Quiet mode until 2pm...' });
+    new Flux('service|mood|set', 0, { delay: 5 });
+  });
+  new CronJob('* 0 14 * * *', function () {
+    new Flux('service|mood|set', HOME_OFFICE_MOOD_LEVEL);
+  });
 }
