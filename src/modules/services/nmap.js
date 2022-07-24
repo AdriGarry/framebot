aca1a5a246f9af56218d318b6482e3f4b40f53bf
AdriGarry <adrigarry@gmail.com>
2022-07-24 19:51:23 +0200

    Phase 2

--- src/modules/services/nmap.js
+++ src/modules/services/nmap.js
@@ -5,36 +5,46 @@
 const nmap = require('node-nmap');
 nmap.nmapLocation = 'nmap';
 
-const { Core, Flux, Logger, Observers, Utils } = require('../../api');
+const { Core, Flux, Logger, Observers, Scheduler, Utils } = require('../../api');
 
 const log = new Logger(__filename);
 
-// module.exports = {
-//   cron: {
-//     full: [{ cron: '0 0 * * * *', flux: { id: 'service|time|now' } }]
-//   }
-// };
+module.exports = {
+  cron: {
+    base: [{ cron: '*/10 * * * * *', flux: { id: 'service|nmap|scan' } }]
+  }
+};
 
-const FLUX_PARSE_OPTIONS = [
-  { id: 'scan', fn: scan } // TODO
-];
+const FLUX_PARSE_OPTIONS = [{ id: 'scan', fn: scan }];
 
 Observers.attachFluxParseOptions('service', 'nmap', FLUX_PARSE_OPTIONS);
 
-setImmediate(() => {
-  scan();
-});
+// setImmediate(() => {
+//   Scheduler.delay(2).then(scan());
+// });
 
-const quickscan = new nmap.QuickScan('192.168.1.0/24'); // Accepts array or comma separated string of NMAP acceptable hosts
+let isScanning = false;
 
 function scan() {
-  quickscan.on('complete', data => {
-    log.test(data);
+  if (isScanning) return;
+
+  log.info('scanning network...');
+  const quickscan = new nmap.QuickScan('192.168.1.0/24'); // Accepts array or comma separated string of NMAP acceptable hosts
+  isScanning = true;
+  quickscan.on('complete', hosts => {
+    isScanning = false;
+    parseSuppliedHosts(hosts);
   });
 
   quickscan.on('error', error => {
+    isScanning = false;
     log.error('Nmap scan error', error);
   });
 
   quickscan.startScan();
+}
+
+function parseSuppliedHosts(hosts) {
+  log.info('parseSuppliedHosts', hosts.length);
+  log.test(hosts);
 }
