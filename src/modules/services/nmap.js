0e63a4637f318ab285a9a43f9ee2bfdcd2fb4da6
AdriGarry <adrigarry@gmail.com>
2022-07-26 23:53:58 +0200

    Improve continuous scan

--- src/modules/services/nmap.js
+++ src/modules/services/nmap.js
@@ -5,23 +5,23 @@
 const nmap = require('node-nmap');
 nmap.nmapLocation = 'nmap';
 
-const { Core, CronJobList, Flux, Logger, Observers, Scheduler, Utils } = require('../../api');
+const { Core, Flux, Logger, Observers, Scheduler, Utils } = require('../../api');
 
 const log = new Logger(__filename);
 
 const FLUX_PARSE_OPTIONS = [
   { id: 'scan', fn: scan },
-  { id: 'scanLoop', fn: scanLoop },
-  { id: 'stopScanLoop', fn: stopScanLoop }
+  { id: 'scanLoop', fn: continuousScan },
+  { id: 'stopScanLoop', fn: stopContinuousScan }
 ];
 
 Observers.attachFluxParseOptions('service', 'nmap', FLUX_PARSE_OPTIONS);
 
 setImmediate(() => {
-  Scheduler.delay(2).then(scan());
+  Scheduler.delay(3).then(scan());
 });
 
-const SCAN_CRON = '*/10 * * * * *',
+const LOCAL_NETWORK_RANGE = '192.16' + '8.1.0/24',
   KNOWN_HOSTS = {
     ADRI: 'Pixel-3a',
     ADRI_PC: 'adri-pc',
@@ -34,27 +34,22 @@ const SCAN_CRON = '*/10 * * * * *',
     NULL: 'null'
   }; //TODO move to descriptor ?
 
-const LOCAL_NETWORK_RANGE = '192.16' + '8.1.0/24',
-  NMAP_JOB = new CronJobList([{ cron: SCAN_CRON, flux: { id: 'service|nmap|scan' } }], 'nmap', true);
-
+let quickScan;
 let hostsList = {},
-  isScanning = false;
+  isContinuousScan = false;
 
 function scan() {
-  if (isScanning) return log.info('Already scanning...');
-
-  let quickScan = new nmap.QuickScan(LOCAL_NETWORK_RANGE); // Accepts array or comma separated string of NMAP acceptable hosts
+  quickScan = new nmap.QuickScan(LOCAL_NETWORK_RANGE);
   quickScan.on('complete', hosts => {
-    isScanning = false;
     parseFoundHosts(hosts);
+    if (isContinuousScan) scan();
   });
 
   quickScan.on('error', error => {
-    isScanning = false;
     log.error('Nmap error:', error);
+    if (isContinuousScan) scan();
   });
 
-  isScanning = true;
   log.info('Nmap scan...');
   quickScan.startScan();
 }
@@ -104,18 +99,19 @@ function newHostReaction(newDetectedHosts) {
   if (unknownHosts.length > 0) new Flux('interface|tts|speak', { lg: 'en', voice: 'mbrolaFr1', msg: 'New unknown host: ' + unknownHosts.join(', ') });
 }
 
-function scanLoop() {
-  log.info('Starting scanLoop...');
+function continuousScan() {
+  log.info('Starting continuous scan...');
   Core.run('nmap', true);
-  NMAP_JOB.start();
+  isContinuousScan = true;
   setTimeout(() => {
-    log.info('Scan loop timeout!');
-    stopScanLoop();
-  }, 2 * 60 * 60 * 1000);
+    log.info('Continuous scan timeout!');
+    stopContinuousScan();
+  }, 60 * 60 * 1000);
+  scan();
 }
 
-function stopScanLoop() {
-  NMAP_JOB.stop();
+function stopContinuousScan() {
+  log.info('Stopping continuous scan...');
   Core.run('nmap', false);
-  log.info('ScanLoop stopped.');
+  isContinuousScan = false;
 }
