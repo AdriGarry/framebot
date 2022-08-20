3617b4970a795324ee5e8140b42f356d2bb7d44c
Adri <adrigarry@gmail.com>
2022-08-20 14:01:17 +0200

    7.13.21 Unknow property on SharedObject (#141)
    * Unlock + remove _open property
    
    * refactor
    
    * Bump version

--- src/core/SharedObject.js
+++ src/core/SharedObject.js
@@ -3,21 +3,20 @@
 
 const fs = require('fs');
 
-const Logger = require('./../api/Logger');
+const Logger = require('../api/Logger');
 
 const log = new Logger(__filename);
 
 /** accessor: object([id, value]) */
-function Lock(obj, filePath, open) {
+function SharedObject(name, obj, filePath) {
   let self = this;
+  self._name = name;
   self._obj = obj;
-  self._open = open;
   if (filePath) {
     self.filePath = filePath;
   } else {
     self.filePath = false;
   }
-  if (!self._open) Object.seal(self);
   return _accessors;
 
   function _accessors(id, newValue) {
@@ -34,7 +33,7 @@ function Lock(obj, filePath, open) {
     } else if (self._obj.hasOwnProperty(id)) {
       return self._obj[id];
     } else {
-      log.error('_getObjValue ERROR:', id);
+      log.error(`No entry for '${id}' property in ${self._name} object!`);
       return false;
     }
   }
@@ -42,31 +41,27 @@ function Lock(obj, filePath, open) {
   function _setter(id, newValue) {
     if (self.filePath) {
       log.debug('Updating: ' + id + '=' + newValue);
-      _setValue(self._obj, id, newValue, self._open);
+      _setValue(self._obj, id, newValue);
       fs.writeFileSync(self.filePath, JSON.stringify(self._obj, null, 1));
     } else {
-      _setValue(self._obj, id, newValue, self._open);
+      _setValue(self._obj, id, newValue);
     }
   }
 
-  function _setValue(object, id, newValue, _open) {
+  function _setValue(object, id, newValue) {
     let id2;
     if (id.indexOf('.') > -1) {
       let keys = id.split('.');
       id = keys[0];
       id2 = keys[1];
     }
-    if (object.hasOwnProperty(id) || _open) {
-      if (Array.isArray(object[id])) {
-        object[id].push(newValue);
-      } else {
-        if (id2) object[id][id2] = newValue;
-        else object[id] = newValue;
-      }
+    if (Array.isArray(object[id])) {
+      object[id].push(newValue);
     } else {
-      log.error('ERROR can not set value for unknow property', id);
+      if (id2) object[id][id2] = newValue;
+      else object[id] = newValue;
     }
   }
 }
 
-module.exports = Lock;
+module.exports = SharedObject;
