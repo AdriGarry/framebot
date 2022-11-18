9aebc40bccc10e354e8ab6e09206672bf2555aa1
Adri <adrigarry@gmail.com>
2022-11-18 10:51:22 +0100

    Add speaker dependency (#149)

--- src/index.js
+++ src/index.js
@@ -48,3 +48,15 @@ if (Core.conf('mode') === 'test') {
     integrationTests.launch();
   }, 1000);
 }
+
+/*const Speaker = require('speaker');
+
+// Create the Speaker instance
+const speaker = new Speaker({
+  channels: 2, // 2 channels
+  bitDepth: 16, // 16-bit samples
+  sampleRate: 44100 // 44,100 Hz sample rate
+});
+
+// PCM data from stdin gets piped into the speaker
+process.stdin.pipe(speaker);*/
