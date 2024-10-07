064906cfcbdbdfb97b043480b8dab7f5bc7832e0
AdriGarry <adrigarry@gmail.com>
2024-10-07 03:28:45 +0200

    7.15.40 Update rfxcom devices icons

--- src/web/js/components.js
+++ src/web/js/components.js
@@ -993,8 +993,8 @@ app.component('powerPlug', {
         { label: 'Plug 3', icon: 'fa-solid fa-plug', value: { device: 'plug3', continu: true } },
         { label: 'Plug 11', icon: 'fa-regular fa-lightbulb fa-rotate-180', value: { device: 'plug11', continu: true } },
         { label: 'Plug 12', icon: 'fa-regular fa-lightbulb', value: { device: 'plug12', continu: true } },
-        { label: 'Plug 13', icon: 'fa-solid fa-user-tie', value: { device: 'plug13', continu: true } },
-        { label: 'Plug 14', icon: 'fa-solid fa-plug', value: { device: 'plug14', continu: true } }
+        { label: 'Plug 13', icon: 'fa-solid fa-plug', value: { device: 'plug13', continu: true } },
+        { label: 'Plug 14', icon: 'fa-solid fa-user-tie', value: { device: 'plug14', continu: true } }
       ]
     };
     ctrl.tile = new DefaultTile(tileParams);
