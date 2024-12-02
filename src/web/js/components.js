6b8af5c3a6f82ea7f7f5b6e1f5a1734d210fc2fe
AdriGarry <adrigarry@gmail.com>
2024-12-02 03:06:53 +0100

    7.16.1 Set Christmass tree icon

--- src/web/js/components.js
+++ src/web/js/components.js
@@ -902,7 +902,8 @@ app.component('powerPlug', {
         { label: 'Plug 2', icon: 'fa-solid fa-plug-circle-bolt', value: { device: 'plug2', continu: true } },
         { label: 'Plug 3', icon: 'fa-solid fa-plug', value: { device: 'plug3', continu: true } },
         { label: 'Plug 11', icon: 'fa-regular fa-sun', value: { device: 'plug11', continu: true } },
-        { label: 'Plug 12', icon: 'fa-regular fa-lightbulb', value: { device: 'plug12', continu: true } },
+        { label: 'Plug 12', icon: 'fa-solid fa-tree', value: { device: 'plug12', continu: true } },
+        // { label: 'Plug 12', icon: 'fa-regular fa-lightbulb', value: { device: 'plug12', continu: true } },
         { label: 'Plug 13', icon: 'fa-solid fa-plug', value: { device: 'plug13', continu: true } },
         { label: 'Plug 14', icon: 'fa-solid fa-user-tie', value: { device: 'plug14', continu: true } }
       ]
