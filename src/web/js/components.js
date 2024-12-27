b9d4016937223254e73998a3f4e39d9f422b46f9
AdriGarry <adrigarry@gmail.com>
2024-12-27 20:23:49 +0100

    7.16.2 Update plug icon

--- src/web/js/components.js
+++ src/web/js/components.js
@@ -900,11 +900,10 @@ app.component('powerPlug', {
       actionList: [
         { label: 'Plug 1', icon: 'fa-solid fa-computer', value: { device: 'plug1', continu: true } },
         { label: 'Plug 2', icon: 'fa-solid fa-plug-circle-bolt', value: { device: 'plug2', continu: true } },
-        { label: 'Plug 3', icon: 'fa-solid fa-plug', value: { device: 'plug3', continu: true } },
+        { label: 'Plug 3', icon: 'fa-solid fa-droplet', value: { device: 'plug3', continu: true } },
         { label: 'Plug 11', icon: 'fa-regular fa-sun', value: { device: 'plug11', continu: true } },
-        { label: 'Plug 12', icon: 'fa-solid fa-tree', value: { device: 'plug12', continu: true } },
-        // { label: 'Plug 12', icon: 'fa-regular fa-lightbulb', value: { device: 'plug12', continu: true } },
-        { label: 'Plug 13', icon: 'fa-solid fa-plug', value: { device: 'plug13', continu: true } },
+        { label: 'Plug 12', icon: 'fa-regular fa-lightbulb', value: { device: 'plug12', continu: true } },
+        { label: 'Plug 13', icon: 'fa-solid fa-tree', value: { device: 'plug13', continu: true } },
         { label: 'Plug 14', icon: 'fa-solid fa-user-tie', value: { device: 'plug14', continu: true } }
       ]
     };
