f558a1d18d92dd5390ea42a103b2d06a70a8d3f5
AdriGarry <adrigarry@gmail.com>
2026-06-10 20:42:17 +0200

    remove TODO

--- src/modules/services/childNightLight.js
+++ src/modules/services/childNightLight.js
@@ -29,7 +29,6 @@ setTimeout(function () {
 function initChildNightLight() {
   log.info('init child night light...');
   new CronJob('15 */10 * * * *', function () {
-    // TODO // '15 0,30 * * * *'
     toggleChildNightLightDependingOnSunPosition();
   }).start();
 }
