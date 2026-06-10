101554b7847dc81f00b8d6bf7f4cfc049f8c80aa
Adri <adrigarry@gmail.com>
2026-06-10 20:43:43 +0200

    7.17.0 Add childNightLight service (#190)
    * Add suncalc dependency
    
    * Add childNightLight service
    
    * WIP
    
    * Fix
    
    * Simplify isChildNightLightOn condition
    
    * Stabilized childNightLight service
    
    * clean code
    
    * check toggleChildNightLightDependingOnSunPosition every 10 min
    
    * remove TODO

---
+++ src/modules/services/childNightLight.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs'),
+  SunCalc = require('suncalc'),
+  CronJob = require('cron').CronJob;
+
+const { Flux, Logger, Observers } = require('./../../api');
+
+const log = new Logger(__filename);
+
+module.exports = {};
+
+const FLUX_PARSE_OPTIONS = [];
+
+Observers.attachFluxParseOptions('service', 'childNightLight', FLUX_PARSE_OPTIONS);
+
+const CALC_SUN_TIMES_DELAY = 24 * 60;
+const LATITUDE = 43.2965;
+const LONGITUDE = 5.3698;
+
+let isChildNightLightOn;
+
+setTimeout(function () {
+  initChildNightLight();
+}, 30 * 1000);
+
+function initChildNightLight() {
+  log.info('init child night light...');
+  new CronJob('15 */10 * * * *', function () {
+    toggleChildNightLightDependingOnSunPosition();
+  }).start();
+}
+
+function toggleChildNightLightDependingOnSunPosition() {
+  log.debug('toggleChildNightLightDependingOnSunPosition...');
+  const now = new Date();
+  const sunTimes = SunCalc.getTimes(now, LATITUDE, LONGITUDE);
+  if (now > sunTimes.sunset) {
+    // night
+    if (isChildNightLightOn != true) {
+      log.info('Switching child night light on...');
+      Flux.do('service|powerPlug|toggle', { plug: 'plug14', mode: true });
+      isChildNightLightOn = true;
+    }
+  } else if (now > sunTimes.sunrise) {
+    // day
+    if (isChildNightLightOn != false) {
+      log.info('Switching child night light off...');
+      Flux.do('service|powerPlug|toggle', { plug: 'plug14', mode: false });
+      isChildNightLightOn = false;
+    }
+  }
+}
